bbdfe72f964f2251fbed812add244290461fedd4
ashdavies <ash.davies@gmail.com>
2015-06-02 01:49:25 +0200

    Update GitHub service to fetch user repositories

--- mobile/src/main/java/com/chaos/databinding/services/GitHub.java
+++ mobile/src/main/java/com/chaos/databinding/services/GitHub.java
@@ -1,5 +1,6 @@
 package com.chaos.databinding.services;
 
+import com.chaos.databinding.models.Repo;
 import com.chaos.databinding.models.User;
 
 import retrofit.http.GET;
@@ -9,5 +10,8 @@ import rx.Observable;
 public interface GitHub {
 
     @GET("/users/{user}")
-    Observable<User> user(@Path("user") final String user);
+    Observable<User> getUser(@Path("getUser") final String user);
+
+    @GET("/users/{user}/repos")
+    Observable<Repo[]> getRepos(@Path("user") final String user);
 }
