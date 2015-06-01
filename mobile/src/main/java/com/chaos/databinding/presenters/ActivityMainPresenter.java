d4b3b1042b80c539ab72f22da8c4f019aec1f4d6
ashdavies <ash.davies@gmail.com>
2015-06-02 01:49:51 +0200

    Populate the listings view with the results of the GitHub service request

--- mobile/src/main/java/com/chaos/databinding/presenters/ActivityMainPresenter.java
+++ mobile/src/main/java/com/chaos/databinding/presenters/ActivityMainPresenter.java
@@ -1,37 +1,42 @@
 package com.chaos.databinding.presenters;
 
-import com.chaos.databinding.models.User;
+import com.chaos.databinding.models.Repo;
 import com.chaos.databinding.services.GitHub;
-import com.chaos.databinding.views.UserView;
+import com.chaos.databinding.views.ListingsView;
 
 import retrofit.RestAdapter;
 import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
-public class ActivityMainPresenter implements Observer<User> {
+public class ActivityMainPresenter implements Observer<Repo[]> {
     private static final String GITHUB_API = "https://api.github.com";
 
     private final GitHub service;
-    private final UserView view;
+    private final ListingsView<Repo> view;
 
-    public ActivityMainPresenter(final GitHub service, final UserView view) {
+    public ActivityMainPresenter(final GitHub service, final ListingsView<Repo> view) {
         this.service = service;
         this.view = view;
     }
 
-    public static ActivityMainPresenter create(final UserView view) {
+    public static ActivityMainPresenter create(final ListingsView<Repo> view) {
         final GitHub service = ActivityMainPresenter.getRestAdapter().create(GitHub.class);
         return new ActivityMainPresenter(service, view);
     }
 
     private static RestAdapter getRestAdapter() {
         return new RestAdapter.Builder()
-                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setLogLevel(RestAdapter.LogLevel.BASIC)
                 .setEndpoint(GITHUB_API)
                 .build();
     }
 
-    public void getUser(final String user) {
-        this.service.user(user).subscribe(this);
+    public void getRepos(final String user) {
+        this.service.getRepos(user)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this);
     }
 
     @Override
@@ -44,7 +49,8 @@ public class ActivityMainPresenter implements Observer<User> {
     }
 
     @Override
-    public void onNext(final User user) {
-        this.view.setUser(user);
+    public void onNext(final Repo[] repos) {
+        this.view.clearItems();
+        this.view.addItems(repos);
     }
 }
