82207ee5996be5d1355ba2e9ad920852c2defffc
ashdavies <ash.davies@gmail.com>
2015-06-02 00:05:24 +0200

    Create presenter with GitHub service

--- mobile/src/main/java/com/chaos/databinding/presenters/ActivityMainPresenter.java
+++ mobile/src/main/java/com/chaos/databinding/presenters/ActivityMainPresenter.java
@@ -1,17 +1,31 @@
 package com.chaos.databinding.presenters;
 
 import com.chaos.databinding.models.User;
+import com.chaos.databinding.services.GitHub;
 import com.chaos.databinding.views.UserView;
 
+import retrofit.RestAdapter;
+
 public class ActivityMainPresenter {
+    private static final String GITHUB_API = "https://api.github.com";
+
+    private final GitHub service;
     private final UserView view;
 
-    public ActivityMainPresenter(final UserView view) {
+    public ActivityMainPresenter(final GitHub service, final UserView view) {
+        this.service = service;
         this.view = view;
     }
 
     public static ActivityMainPresenter create(final UserView view) {
-        return new ActivityMainPresenter(view);
+        final GitHub service = ActivityMainPresenter.getRestAdapter().create(GitHub.class);
+        return new ActivityMainPresenter(service, view);
+    }
+
+    private static RestAdapter getRestAdapter() {
+        return new RestAdapter.Builder()
+                .setEndpoint(GITHUB_API)
+                .build();
     }
 
     public void updateUser() {
