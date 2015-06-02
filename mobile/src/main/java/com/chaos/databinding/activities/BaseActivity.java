b760a0e45627c748836b61db0e5b0b15c5355bc8
ashdavies <ash.davies@gmail.com>
2015-06-02 14:56:20 +0200

    Include v7 toolbar with base activity abstract to fetch identifier to swap out support toolbar

--- mobile/src/main/java/com/chaos/databinding/activities/BaseActivity.java
+++ mobile/src/main/java/com/chaos/databinding/activities/BaseActivity.java
@@ -1,7 +1,10 @@
 package com.chaos.databinding.activities;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
@@ -9,15 +12,22 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        int layoutId = onCreateViewId();
+        final int layoutId = this.onCreateViewId();
         this.setContentView(layoutId);
+
+        final int toolbarId = this.onCreateViewToolbarId();
+        this.setSupportActionBar((Toolbar)this.findViewById(toolbarId));
     }
 
     @Override
-    public void setContentView(int layoutResID) {
-        super.setContentView(layoutResID);
+    public void setContentView(int layoutResId) {
+        super.setContentView(layoutResId);
         //ButterKnife.inject(this);
     }
 
+    @LayoutRes
     protected abstract int onCreateViewId();
+
+    @IdRes
+    protected abstract int onCreateViewToolbarId();
 }
