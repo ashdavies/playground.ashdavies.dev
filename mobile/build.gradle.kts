232b19f2aba76af0214aed7640de5983a51be203
Ash Davies <ashdavies@users.noreply.github.com>
2019-11-12 18:33:37 +0100

    Include firestore ktx dependencies

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -65,8 +65,9 @@ dependencies {
 
   implementation("com.github.ashdavies:lifecycle-flow:0.7.2")
   implementation("com.google.android.material:material:1.2.0-alpha01")
+  implementation("com.google.firebase:firebase-common-ktx:19.2.0")
   implementation("com.google.firebase:firebase-analytics:17.2.1")
-  implementation("com.google.firebase:firebase-database:19.2.0")
+  implementation("com.google.firebase:firebase-firestore-ktx:21.2.1")
 
   implementation("com.squareup.moshi:moshi:1.9.1")
   implementation("com.squareup.okhttp3:logging-interceptor:4.2.2")
@@ -77,6 +78,7 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.60-eap-143")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.2")
   implementation("org.reduxkotlin:redux-kotlin-jvm:0.2.6")
 
   kapt("androidx.room:room-compiler:2.2.1")
