950ca5429d09308b554d4673e227f08d4543f345
Ash Davies <ashdavies@users.noreply.github.com>
2021-02-02 22:43:15 +0100

    Include dependencies for npm

--- functions/build.gradle.kts
+++ functions/build.gradle.kts
@@ -20,6 +20,13 @@ kotlin {
 dependencies {
     implementation(JetBrains.KotlinX.kotlinxDatetime)
     implementation(JetBrains.KotlinX.kotlinxNodejs)
+
+    implementation(npm("@octokit/graphql", "4.5.8"))
+    implementation(npm("@types/humps", "2.0.0"))
+    implementation(npm("firebase-admin", "9.4.1"))
+    implementation(npm("firebase-functions", "3.11.0"))
+    implementation(npm("firestore", "1.1.6"))
+    implementation(npm("yaml", "1.10.0"))
 }
 
 tasks.withType<KotlinWebpack> {
