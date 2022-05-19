66b415d56cfbd9e43878443bc076dbe9ac309f07
Davies Ashley <ashdavies@users.noreply.github.com>
2022-05-19 23:12:45 +0200

    Inline default screen behaviour into compose activity

--- playground-app/src/androidMain/kotlin/io/ashdavies/playground/ComposeActivity.kt
+++ playground-app/src/androidMain/kotlin/io/ashdavies/playground/ComposeActivity.kt
@@ -3,10 +3,15 @@ package io.ashdavies.playground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat.setDecorFitsSystemWindows
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.defaultComponentContext
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 abstract class ComposeActivity(
     private val decorFitsSystemWindows: Boolean = false,
@@ -17,7 +22,25 @@ abstract class ComposeActivity(
         super.onCreate(savedInstanceState)
 
         setDecorFitsSystemWindows(window, decorFitsSystemWindows)
-        setContent { content(defaultComponentContext()) }
+        setContent { ComposeScreen { content(defaultComponentContext()) } }
     }
 }
 
+@Composable
+private fun ComposeScreen(content: @Composable () -> Unit) {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = !isSystemInDarkTheme()
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = useDarkIcons,
+        )
+    }
+
+    PlaygroundTheme {
+        ProvideWindowInsets {
+            content()
+        }
+    }
+}
