f6edb6d32b7fdcb78b46ea3816e9aa16b7632f50
Davies Ashley <ashdavies@users.noreply.github.com>
2022-06-15 18:35:38 +0200

    Provide fake for events test

--- events-function/src/test/kotlin/io/ashdavies/playground/events/EventsFunctionTest.kt
+++ events-function/src/test/kotlin/io/ashdavies/playground/events/EventsFunctionTest.kt
@@ -6,10 +6,11 @@ import kotlin.test.Test
 internal class EventsFunctionTest {
 
     private val request = FakeHttpRequest.build()
+    private val response = FakeHttpResponse()
     private val function = EventsFunction()
 
     @Test(expected = IllegalStateException::class)
     fun test() = runBlocking<Unit> {
-        function.service(request)
+        function.service(request, response)
     }
 }
