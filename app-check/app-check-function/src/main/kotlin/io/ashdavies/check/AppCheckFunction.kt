b3b6a95631c84a8fa087ad8a3d6ab05b81ccc54a
Davies Ashley <ashdavies@users.noreply.github.com>
2022-10-13 08:34:14 +0200

    Simplify function

--- app-check/app-check-function/src/main/kotlin/io/ashdavies/check/AppCheckFunction.kt
+++ app-check/app-check-function/src/main/kotlin/io/ashdavies/check/AppCheckFunction.kt
@@ -8,13 +8,9 @@ import io.ashdavies.compose.rememberAppCheck
 import io.ashdavies.compose.rememberCryptoSigner
 import io.ashdavies.compose.rememberProjectId
 import io.ashdavies.playground.cloud.HttpEffect
-import io.ashdavies.playground.cloud.HttpException
 import io.ashdavies.playground.cloud.LocalHttpRequest
 import kotlinx.datetime.Clock.System.now
 import kotlin.time.Duration.Companion.hours
-
-private const val APP_CHECK_KEY = "APP_CHECK_KEY"
-private const val BAD_AUTHENTICITY = "Bad authenticity"
 
 internal class AppCheckFunction : HttpFunction by AuthorisedHttpApplication({
     val request: HttpRequest = LocalHttpRequest.current
@@ -27,10 +23,6 @@ internal class AppCheckFunction : HttpFunction by AuthorisedHttpApplication({
     val projectId: String = rememberProjectId()
 
     HttpEffect {
-        if (query.appKey != System.getenv(APP_CHECK_KEY)) {
-            throw HttpException.Forbidden(BAD_AUTHENTICITY)
-        }
-
         val token = AppCheckToken.Request.Raw(projectId, query.appId)
         val response = appCheck.createToken(token) {
             it.issuer = signer.getAccountId()
--- app-check/app-check-js/index.js
+++ app-check/app-check-js/index.js
@@ -5,14 +5,20 @@ firebaseAdmin.initializeApp();
 
 exports.createToken = functions.region("europe-west1").https.onRequest((req, res) => {
     firebaseAdmin.appCheck().createToken(req.query.appId)
-        .then((token) => { return res.send({ expiresAt: Math.floor(Date.now() / 1000) + 60 * 60, token: token }) })
-        .catch((err) => { res.status(401).send(err.message) })
+        .then((token) => {
+            return res.send({expiresAt: Math.floor(Date.now() / 1000) + 60 * 60, token: token})
+        })
+        .catch((err) => {
+            res.status(500).send(err.message)
+        })
 });
 
 exports.verifyToken = functions.region("europe-west1").https.onRequest((req, res) => {
-    const token = req.header("X-Firebase-AppCheck")
-
-    firebaseAdmin.appCheck().verifyToken(token)
-        .then((tokenRes) => { return res.status(200).send(tokenRes.token) })
-        .catch((err) => { res.status(401).send(err.message) })
+    firebaseAdmin.appCheck().verifyToken(req.header("X-Firebase-AppCheck"))
+        .then((tokenRes) => {
+            return res.status(200).send(tokenRes.token)
+        })
+        .catch((err) => {
+            res.status(401).send(err.message)
+        })
 });
