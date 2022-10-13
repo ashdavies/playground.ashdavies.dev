34f216dd61a2a5788a0dbe2d15e9b29d189a783f
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2022-10-13 08:35:06 +0200

    Firebase AppCheck JS SDK (#33)
    * Init Firebase functions app check token
    
    * Create niave implementation of crypto signer
    
    * Resolve project identifier and IAM endpoint for signing
    
    * Remove lock files (not recommended)
    
    * Restore Google Cloud GitHub actions
    
    * Disable documented encryption steps
    
    * Include run key in parent yaml declaration
    
    * Dynamically retrieve application service account identifier
    
    * Explicitly log and rethrow content for HttpApplication
    
    * Resolve issues in dominion app
    
    * Remove exception and include baseline test function
    
    * Include additional test function
    
    * Prepare test function for litmus checking
    
    * Test more variant types to isolate error
    
    * Adjust test to verify bearer token retrieval
    
    * Include more diagnostic tests
    
    * Include more diagnostic tests for header application
    
    * Include manual coroutine request
    
    * Wrap coroutine expectation in try catch
    
    * Wrap entire async execution bloc in catch
    
    * Enable all disabled tests
    
    * Add test results ass comments on test
    
    * Adjust test explicitly for bearer token
    
    * Remove diagnostics, increase output logging
    
    * Roll back molecule plugin version
    
    * Simplify test configuration for unauthorised setup
    
    * Wrap http effect in exception handling
    
    * Restore direct coroutine invocation of bearer response
    
    * Include access token for jwt request
    
    * Roll back diagnostics tests
    
    * Introduce HttpScope for effect functions
    
    * Restore sample index implementation
    
    * Resolve permission denied failure in app check function with correction of public jwks url
    
    * Move js implementation to app-check folder
    
    * Simplify function
    
    Co-authored-by: Davies Ashley <ashdavies@users.noreply.github.com>

---
+++ app-check/app-check-js/index.js
@@ -0,0 +1,24 @@
+const functions = require("firebase-functions");
+const firebaseAdmin = require("firebase-admin");
+
+firebaseAdmin.initializeApp();
+
+exports.createToken = functions.region("europe-west1").https.onRequest((req, res) => {
+    firebaseAdmin.appCheck().createToken(req.query.appId)
+        .then((token) => {
+            return res.send({expiresAt: Math.floor(Date.now() / 1000) + 60 * 60, token: token})
+        })
+        .catch((err) => {
+            res.status(500).send(err.message)
+        })
+});
+
+exports.verifyToken = functions.region("europe-west1").https.onRequest((req, res) => {
+    firebaseAdmin.appCheck().verifyToken(req.header("X-Firebase-AppCheck"))
+        .then((tokenRes) => {
+            return res.status(200).send(tokenRes.token)
+        })
+        .catch((err) => {
+            res.status(401).send(err.message)
+        })
+});
