221452653040f09c95267ba1788527e18e55290d
Davies Ashley <ashdavies@users.noreply.github.com>
2022-10-16 17:02:54 +0200

    Update service names

--- app-check/app-check-tf/main.tf
+++ app-check/app-check-tf/main.tf
@@ -4,33 +4,34 @@ provider "github" {
 }
 
 resource "google_service_account" "sa" {
+  display_name = "GitHub Service Account"
   project    = var.project_id
   account_id = "gh-oidc"
 }
 
 resource "google_project_iam_member" "project" {
-  project = var.project_id
-  role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.sa.email}"
+  role    = "roles/storage.admin"
+  project = var.project_id
 }
 
 resource "github_actions_secret" "google_service_account_id" {
-  repository      = var.gh_repo_name
-  secret_name     = "google_service_account_id"
   plaintext_value = google_service_account.sa.email
+  secret_name     = "google_service_account_id"
+  repository      = var.gh_repo_name
 }
 
 resource "github_actions_secret" "google_workload_identity" {
-  repository      = var.gh_repo_name
+  plaintext_value = module.gh-oidc.provider_name
   secret_name     = "google_workload_identity"
-  plaintext_value = ""
+  repository      = var.gh_repo_name
 }
 
 module "gh-oidc" {
   source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  provider_id = "gh-oidc-provider"
   project_id  = var.project_id
   pool_id     = "gh-oidc-pool"
-  provider_id = "gh-oidc-provider"
   sa_mapping = {
     (google_service_account.sa.account_id) = {
       sa_name   = google_service_account.sa.name
--- app-check/app-check-tf/outputs.tf
+++ app-check/app-check-tf/outputs.tf
@@ -1,14 +1,14 @@
 output "pool_name" {
-  description = "Pool name"
+  description = "Workload Identity Pool name"
   value       = module.gh-oidc.pool_name
 }
 
 output "provider_name" {
-  description = "Provider name"
+  description = "Workload Identity Provider name"
   value       = module.gh-oidc.provider_name
 }
 
 output "sa_email" {
-  description = "Example SA email"
+  description = "Google Service Account email"
   value       = google_service_account.sa.email
 }
