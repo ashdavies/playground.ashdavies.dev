f6fa867fe3b28fd25a016d634db757866f3c5acf
Davies Ashley <ashdavies@users.noreply.github.com>
2022-10-17 23:10:29 +0200

    Invert test assertion

--- app-check/app-check-function/src/test/kotlin/io/ashdavies/check/AppCheckFunctionTest.kt
+++ app-check/app-check-function/src/test/kotlin/io/ashdavies/check/AppCheckFunctionTest.kt
@@ -7,6 +7,7 @@ import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class AppCheckFunctionTest {
@@ -17,5 +18,14 @@ internal class AppCheckFunctionTest {
         val request: HttpResponse = client.get { parameter("appId", mobileSdkAppId) }
 
         assertEquals(HttpStatusCode.OK, request.status)
+    }
+
+    @Test
+    fun `should verify environment variables`() {
+        val googleServiceAccountId = System.getenv("GOOGLE_SERVICE_ACCOUNT_ID")
+        assertTrue(googleServiceAccountId.isEmpty())
+
+        val googleWorkloadIdentity = System.getenv("GOOGLE_WORKLOAD_IDENTITY")
+        assertTrue(googleWorkloadIdentity.isEmpty())
     }
 }
