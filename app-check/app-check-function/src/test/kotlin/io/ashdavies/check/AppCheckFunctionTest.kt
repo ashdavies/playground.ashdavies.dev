ee8e0def8569a17bafc2b56ac9a184db3e9374ce
Davies Ashley <ashdavies@users.noreply.github.com>
2022-10-17 23:15:42 +0200

    Synchronise environment variable names

--- app-check/app-check-function/src/test/kotlin/io/ashdavies/check/AppCheckFunctionTest.kt
+++ app-check/app-check-function/src/test/kotlin/io/ashdavies/check/AppCheckFunctionTest.kt
@@ -7,7 +7,6 @@ import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class AppCheckFunctionTest {
@@ -18,14 +17,5 @@ internal class AppCheckFunctionTest {
         val request: HttpResponse = client.get { parameter("appId", mobileSdkAppId) }
 
         assertEquals(HttpStatusCode.OK, request.status)
-    }
-
-    @Test
-    fun `should verify environment variables`() {
-        val googleServiceAccountId = System.getenv("GOOGLE_SERVICE_ACCOUNT_ID")
-        assertTrue(googleServiceAccountId.isEmpty())
-
-        val googleWorkloadIdentity = System.getenv("GOOGLE_WORKLOAD_IDENTITY")
-        assertTrue(googleWorkloadIdentity.isEmpty())
     }
 }
