8c737aa8aac028c403a09a6cc47468194e65ae16
Davies Ashley <ashdavies@users.noreply.github.com>
2022-10-21 19:09:20 +0200

    Move request parsing to instantiation

--- app-check/app-check-function/src/main/kotlin/io/ashdavies/check/AppCheckFunction.kt
+++ app-check/app-check-function/src/main/kotlin/io/ashdavies/check/AppCheckFunction.kt
@@ -12,19 +12,16 @@ import io.ashdavies.playground.cloud.LocalFirebaseApp
 import io.ashdavies.playground.cloud.LocalHttpRequest
 import io.ktor.client.HttpClient
 import kotlinx.datetime.Clock.System.now
-import java.net.URLDecoder
-import java.nio.charset.StandardCharsets
 import kotlin.time.Duration.Companion.hours
 
 internal class AppCheckFunction : HttpFunction by AuthorisedHttpApplication({
-    val appCheckQuery = rememberAppCheckQuery()
+    val appCheckRequest = rememberAppCheckRequest()
     val cryptoSigner = rememberCryptoSigner()
     val projectId = rememberProjectId()
     val appCheck = rememberAppCheck()
 
     HttpEffect {
-        val appId = URLDecoder.decode(appCheckQuery.appId, StandardCharsets.UTF_8.name())
-        val token = AppCheckToken.Request.Raw(projectId, appId)
+        val token = AppCheckToken.Request.Raw(projectId, appCheckRequest.appId)
         val response = appCheck.createToken(token) {
             it.issuer = cryptoSigner.getAccountId()
             it.expiresAt = now() + 1.hours
@@ -36,10 +33,10 @@ internal class AppCheckFunction : HttpFunction by AuthorisedHttpApplication({
 })
 
 @Composable
-private fun rememberAppCheckQuery(
+private fun rememberAppCheckRequest(
     httpRequest: HttpRequest = LocalHttpRequest.current
-): AppCheckQuery = remember(httpRequest) {
-    AppCheckQuery(httpRequest)
+): AppCheckRequest = remember(httpRequest) {
+    AppCheckRequest(httpRequest)
 }
 
 @Composable
--- app-check/app-check-function/src/test/kotlin/io/ashdavies/check/AppCheckFunctionTest.kt
+++ app-check/app-check-function/src/test/kotlin/io/ashdavies/check/AppCheckFunctionTest.kt
@@ -1,9 +1,10 @@
 package io.ashdavies.check
 
-import io.ktor.client.request.get
-import io.ktor.client.request.parameter
-import io.ktor.client.statement.HttpResponse
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -13,9 +14,13 @@ internal class AppCheckFunctionTest {
 
     @Test
     fun `should return app check token for given credentials`() = startServer<AppCheckFunction> { client ->
-        val mobileSdkAppId: String = requireNotNull(System.getenv("MOBILE_SDK_APP_ID"))
-        val request: HttpResponse = client.get { parameter("appId", mobileSdkAppId) }
+        val mobileSdkAppId = requireNotNull(System.getenv("MOBILE_SDK_APP_ID"))
 
-        assertEquals(HttpStatusCode.OK, request.status)
+        val response = client.post {
+            contentType(ContentType.Application.Json)
+            setBody(AppCheckRequest(mobileSdkAppId))
+        }
+
+        assertEquals(HttpStatusCode.OK, response.status)
     }
 }
