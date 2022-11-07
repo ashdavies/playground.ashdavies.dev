cbc64faf8f4808d4c3e1522410264afb788b8ea1
Davies Ashley <ashdavies@users.noreply.github.com>
2022-11-07 17:42:49 +0100

    Include event aggregator function in terraform configuration

--- google-cloud/src/main/terraform/main.tf
+++ google-cloud/src/main/terraform/main.tf
@@ -38,6 +38,28 @@ resource "google_project_iam_member" "gh_service_account" {
   role    = each.key
 }
 
+module "create-token" {
+  // source_file          = "../../../build/terraform/main/runtimeExecution/resources/app-check-function-all.jar"
+  function_description = "Firebase AppCheck function to create a new token"
+  source_file          = var.resources.app-check-function-all_jar.path
+  entry_point          = "io.ashdavies.check.AppCheckFunction"
+  source               = "./modules/gcp-function"
+  project_region       = var.project_region
+  project_id           = var.project_id
+  function_name        = "create-token"
+}
+
+module "aggregate-events" {
+  // source_file          = "../../../build/terraform/main/runtimeExecution/resources/events-aggregator-all.jar"
+  entry_point          = "io.ashdavies.playground.aggregator.AggregatorFunction"
+  source_file          = var.resources.events-aggregator-all_jar.path
+  function_description = "Google Cloud Function to aggregate events"
+  source               = "./modules/gcp-function"
+  project_region       = var.project_region
+  function_name        = "aggregate-events"
+  project_id           = var.project_id
+}
+
 module "gh-oidc" {
   source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
   provider_id = "gh-oidc-provider"
@@ -49,15 +71,4 @@ module "gh-oidc" {
       sa_name   = google_service_account.gh_service_account.name
     }
   }
-}
-
-module "cloud-function" {
-  function_description = "Create a new Google Cloud Function"
-  // source_file          = var.resources.app-check-function-all_jar.path
-  source_file          = "../../../build/terraform/main/runtimeExecution/resources/app-check-function-all.jar"
-  entry_point          = "io.ashdavies.check.AppCheckFunction"
-  source               = "./modules/gcp-function"
-  project_region       = var.project_region
-  project_id           = var.project_id
-  function_name        = "create-token"
 }
