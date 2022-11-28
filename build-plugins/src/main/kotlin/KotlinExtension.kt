9d3c80e820ceff23701bfdbc704212cc4842be0d
Davies Ashley <ashdavies@users.noreply.github.com>
2022-11-28 21:41:41 +0100

    Collapse extension function to kotlin convention plugin

--- build-plugins/src/main/kotlin/KotlinExtension.kt
+++ build-plugins/src/main/kotlin/KotlinExtension.kt
@@ -4,15 +4,12 @@ import org.gradle.api.NamedDomainObjectCollection
 import org.gradle.api.Project
 import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.api.file.FileTree
+import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.api.provider.Provider
 import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.getting
 import org.gradle.kotlin.dsl.provideDelegate
-import org.jetbrains.compose.ComposePlugin
-import org.jetbrains.compose.ExperimentalComposeLibrary
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import java.io.File
@@ -28,78 +25,16 @@ public object Playground {
     )
 }
 
-internal fun CommonExtension<*, *, *, *>.configureCommon() {
-    buildFeatures {
-        compose = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    defaultConfig {
-        compileSdk = 33
-        minSdk = 21
-    }
-
-    sourceSets.configureEach {
-        manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    }
-}
-
-internal fun KotlinProjectExtension.configureKotlinProject(target: Project) = target.run {
-    sourceSets.all { languageSettings.optIn("kotlin.RequiresOptIn") }
-    explicitApiWarning()
-}
-
-@Suppress("UNUSED_VARIABLE")
-@OptIn(ExperimentalComposeLibrary::class)
-internal fun KotlinMultiplatformExtension.configureKotlinMultiplatform(target: Project) = target.run {
-    android()
-    jvm()
-
-    val compose = ComposePlugin.Dependencies
-
-    val commonMain by sourceSets.dependencies {
-        implementation(compose.foundation)
-        implementation(compose.material3)
-        implementation(compose.runtime)
-        implementation(compose.uiTooling)
-        implementation(compose.ui)
-
-        implementation(libs.bundles.arkivanov.decompose)
-        implementation(libs.bundles.jetbrains.kotlinx)
-        implementation(libs.oolong)
-    }
-
-    val commonTest by sourceSets.dependencies {
-        implementation(libs.bundles.jetbrains.kotlin.test)
-    }
-
-    val androidMain by sourceSets.dependencies {
-        implementation(libs.androidx.annotation)
-        implementation(libs.androidx.core.ktx)
-        implementation(libs.google.android.material)
-        implementation(libs.jetbrains.kotlinx.coroutines.android)
-    }
-
-    val androidTest by sourceSets.getting {
-        val androidAndroidTestRelease by sourceSets.getting
-        dependsOn(androidAndroidTestRelease)
-    }
-
-    val androidTestFixtures by sourceSets.getting
-    val androidTestFixturesDebug by sourceSets.getting
-    val androidTestFixturesRelease by sourceSets.getting
-
-    val jvmMain by sourceSets.dependencies {
-        implementation(compose.desktop.currentOs)
-        implementation(libs.jetbrains.kotlinx.coroutines.swing)
-    }
-}
-
 public fun Jar.dependency(provider: Provider<MinimalExternalModuleDependency>): List<FileTree> {
+    fun MinimalExternalModuleDependency.matches(tree: FileTree): Boolean {
+        val version = tree.firstOrNull { it.extension == "version" } ?: return false
+        return version.name.contains(module.group)
+    }
+
+    fun fileTree(project: Project, file: File): FileTree {
+        return if (file.isDirectory) file as FileTree else project.zipTree(file)
+    }
+
     val jvmRuntimeClasspath by project.configurations
     val dependency = provider.get()
 
@@ -112,15 +47,10 @@ public fun Jar.dependency(provider: Provider<MinimalExternalModuleDependency>): 
     }
 }
 
-private fun MinimalExternalModuleDependency.matches(tree: FileTree): Boolean {
-    val version = tree.firstOrNull { it.extension == "version" } ?: return false
-    return version.name.contains(module.group)
-}
-
-private fun fileTree(project: Project, file: File): FileTree {
-    return if (file.isDirectory) file as FileTree else project.zipTree(file)
-}
-
 public fun NamedDomainObjectCollection<KotlinSourceSet>.dependencies(
     configure: KotlinDependencyHandler.() -> Unit
 ) = getting { dependencies(configure) }
+
+public inline fun <reified T> ExtensionContainer.withExtension(configure: T.() -> Unit) {
+    findByType(T::class.java)?.configure()
+}
--- build-plugins/src/main/kotlin/io.ashdavies.kotlin.gradle.kts
+++ build-plugins/src/main/kotlin/io.ashdavies.kotlin.gradle.kts
@@ -1,6 +1,11 @@
 import com.android.build.api.dsl.CommonExtension
 import com.android.build.gradle.LibraryExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import org.gradle.api.JavaVersion
+import org.gradle.kotlin.dsl.getValue
+import org.gradle.kotlin.dsl.getting
+import org.jetbrains.compose.ComposePlugin
+import org.jetbrains.compose.ExperimentalComposeLibrary
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 pluginManager.withPlugin("android") {
@@ -14,11 +19,79 @@ plugins {
     kotlin("plugin.serialization")
 }
 
-extensions.configure<LibraryExtension>(CommonExtension<*, *, *, *>::configureCommon)
+kotlin {
+    explicitApiWarning()
+    android()
+    jvm()
 
-extensions.configure<KotlinMultiplatformExtension> {
-    configureKotlinMultiplatform(project)
-    configureKotlinProject(project)
+    sourceSets.all {
+        languageSettings.optIn("kotlin.RequiresOptIn")
+    }
+
+    val compose = ComposePlugin.Dependencies
+
+    @Suppress("UNUSED_VARIABLE")
+    @OptIn(ExperimentalComposeLibrary::class)
+    val commonMain by sourceSets.dependencies {
+        implementation(compose.foundation)
+        implementation(compose.material3)
+        implementation(compose.runtime)
+        implementation(compose.uiTooling)
+        implementation(compose.ui)
+
+        implementation(libs.bundles.arkivanov.decompose)
+        implementation(libs.bundles.jetbrains.kotlinx)
+        implementation(libs.oolong)
+    }
+
+    @Suppress("UNUSED_VARIABLE")
+    val commonTest by sourceSets.dependencies {
+        implementation(libs.bundles.jetbrains.kotlin.test)
+    }
+
+    @Suppress("UNUSED_VARIABLE")
+    val androidMain by sourceSets.dependencies {
+        implementation(libs.androidx.annotation)
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.google.android.material)
+        implementation(libs.jetbrains.kotlinx.coroutines.android)
+    }
+
+    @Suppress("UNUSED_VARIABLE")
+    val androidTest by sourceSets.getting {
+        val androidAndroidTestDebug by sourceSets.getting
+        dependsOn(androidAndroidTestDebug)
+    }
+
+    @Suppress("UNUSED_VARIABLE")
+    val jvmMain by sourceSets.dependencies {
+        implementation(compose.desktop.currentOs)
+        implementation(libs.jetbrains.kotlinx.coroutines.swing)
+    }
+}
+
+extensions.withExtension<BaseAppModuleExtension> { configure() }
+
+extensions.withExtension<LibraryExtension> { configure() }
+
+fun CommonExtension<*, *, *, *>.configure() {
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    defaultConfig {
+        compileSdk = 33
+        minSdk = 21
+    }
+
+    sourceSets.configureEach {
+        manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    }
 }
 
 tasks.withType<KotlinCompile> {
