b0dae1f835461f411765ed392dcae1aeeb0f5592
Davies Ashley <ashdavies@users.noreply.github.com>
2022-12-06 16:58:25 +0100

    Use extension function for agnostic configuration

--- build-plugins/build.gradle.kts
+++ build-plugins/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 
+<<<<<<< Updated upstream
     plugin(libs.plugins.android.library)
     plugin(libs.plugins.apollo.graphql)
     plugin(libs.plugins.kotlin.compose)
@@ -12,6 +13,21 @@ dependencies {
     plugin(libs.plugins.kotlin.serialization)
     plugin(libs.plugins.johnrengelman.shadow)
     plugin(libs.plugins.squareup.sqldelight)
+=======
+    fun plugin(provider: Provider<PluginDependency>) = with(provider.get()) {
+        "$pluginId:$pluginId.gradle.plugin:$version"
+    }
+
+    with(libs.plugins) {
+        implementation(plugin(android.library))
+        implementation(plugin(apollo.graphql))
+        implementation(plugin(kotlin.compose))
+        implementation(plugin(kotlin.multiplatform))
+        implementation(plugin(kotlin.serialization))
+        implementation(plugin(johnrengelman.shadow))
+        implementation(plugin(squareup.sqldelight))
+    }
+>>>>>>> Stashed changes
 }
 
 fun DependencyHandler.plugin(provider: Provider<PluginDependency>): Dependency? {
