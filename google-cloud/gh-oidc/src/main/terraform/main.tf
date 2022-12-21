93c3560e885e5c7780fd148f5d3325c3b4ddaebd
Davies Ashley <ashdavies@users.noreply.github.com>
2022-12-21 14:52:03 +0100

    Include additional service account roles

--- google-cloud/gh-oidc/src/main/terraform/main.tf
+++ google-cloud/gh-oidc/src/main/terraform/main.tf
@@ -1,12 +1,10 @@
 resource "google_project_iam_member" "gh_service_account" {
   for_each = toset([
-    "roles/iam.serviceAccountTokenCreator", // Deprecated
-    "roles/servicemanagement.configEditor"
+    "roles/servicemanagement.configEditor",
     "roles/iam.workloadIdentityUser",
     "roles/artifactregistry.writer",
-    "roles/iam.serviceAccountUser", // Deprecated
-    "roles/run.developer",
-    "roles/storage.admin", // Deprecated
+    "roles/storage.objectViewer",
+    "roles/run.admin",
   ])
 
   member  = "serviceAccount:${google_service_account.gh_service_account.email}"
