5c40cedff5173dd332b3d69b944f41a81d8086ef
Davies Ashley <ashdavies@users.noreply.github.com>
2022-12-21 19:22:01 +0100

    Respond OK

--- cloud-run/src/main/kotlin/io/ashdavies/cloud/Token.kt
+++ cloud-run/src/main/kotlin/io/ashdavies/cloud/Token.kt
@@ -1,5 +1,6 @@
 package io.ashdavies.cloud
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Routing
@@ -7,6 +8,6 @@ import io.ktor.server.routing.post
 
 internal fun Routing.token() {
     post("/token") {
-        call.respond("Hello, World!")
+        call.respond(HttpStatusCode.OK)
     }
 }
