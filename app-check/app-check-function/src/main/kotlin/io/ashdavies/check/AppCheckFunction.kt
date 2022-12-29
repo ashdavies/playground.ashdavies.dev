b5bd38286f3b05422fb4a0212a1129649af865bc
Davies Ashley <ashdavies@users.noreply.github.com>
2022-12-29 19:52:34 +0100

    Remove caching of app check request

--- app-check/app-check-function/src/main/kotlin/io/ashdavies/check/AppCheckFunction.kt
+++ app-check/app-check-function/src/main/kotlin/io/ashdavies/check/AppCheckFunction.kt
@@ -12,21 +12,25 @@ import io.ashdavies.playground.cloud.HttpScope
 import io.ashdavies.playground.cloud.LocalFirebaseAdminApp
 import io.ashdavies.playground.cloud.LocalHttpRequest
 import io.ktor.client.HttpClient
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
 
+@OptIn(ExperimentalSerializationApi::class)
 internal class AppCheckFunction : HttpFunction by HttpApplication(
     config = HttpConfig.Post,
     block = { appCheck() },
 )
 
 @Composable
+@ExperimentalSerializationApi
 private fun HttpScope.appCheck(
     firebaseApp: FirebaseApp = LocalFirebaseAdminApp.current,
     httpRequest: HttpRequest = LocalHttpRequest.current,
     httpClient: HttpClient = LocalHttpClient.current,
 ) = HttpEffect {
-    val appCheckRequest = AppCheckRequest(httpRequest)
+    val appCheckRequest = Json.decodeFromStream<AppCheckRequest>(httpRequest.inputStream)
     val appCheck = firebaseApp.appCheck(httpClient)
 
     val response = appCheck.createToken(
