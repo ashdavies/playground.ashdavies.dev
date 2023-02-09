35e61efaa08775db124b9cc1a45e968a5806ea0b
Davies Ashley <ashdavies@users.noreply.github.com>
2023-02-09 19:38:39 +0100

    Include verifications and build adjustments to debug service account access token failure

--- app-check/app-check-sdk/src/test/kotlin/io/ashdavies/check/CryptoSignerTest.kt
+++ app-check/app-check-sdk/src/test/kotlin/io/ashdavies/check/CryptoSignerTest.kt
@@ -1,5 +1,8 @@
 package io.ashdavies.check
 
+import com.google.auth.oauth2.ImpersonatedCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.internal.EmulatorCredentials
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
@@ -20,6 +23,12 @@ internal class CryptoSignerTest {
 
     @Test
     fun `should refresh access token with impersonated credentials`() = runTest {
+        val firebaseOptions = firebaseApp.options.toBuilder()
+            .setServiceAccountId("gh-oidc@playground-1a136.iam.gserviceaccount.com")
+            .setCredentials(ImpersonatedCredentials.create())
+            .build()
+
+        val firebaseApp = FirebaseApp.initializeApp(firebaseOptions, "[SECONDARY]")
         val cryptoSigner = CryptoSigner(firebaseApp)
         val randomUuid = "${UUID.randomUUID()}"
 
