a7eac6d733023a3b555de8a171f008f3c03fc8e0
Davies Ashley <ashdavies@users.noreply.github.com>
2023-02-09 20:05:16 +0100

    Remove diagnostic tests

--- app-check/app-check-sdk/src/test/kotlin/io/ashdavies/check/FirebaseApp.kt
+++
@@ -1,7 +0,0 @@
-package io.ashdavies.check
-
-import com.google.firebase.FirebaseApp
-
-internal val firebaseApp: FirebaseApp by lazy(LazyThreadSafetyMode.NONE) {
-    FirebaseApp.initializeApp()
-}
