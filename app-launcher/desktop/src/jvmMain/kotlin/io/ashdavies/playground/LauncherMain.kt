bf54a251470c975b7c2e04337f56311b923d5aac
Davies Ashley <ashdavies@users.noreply.github.com>
2023-02-13 20:56:56 +0100

    Update desktop app references

--- app-launcher/desktop/src/jvmMain/kotlin/io/ashdavies/playground/LauncherMain.kt
+++ app-launcher/desktop/src/jvmMain/kotlin/io/ashdavies/playground/LauncherMain.kt
@@ -5,23 +5,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import com.arkivanov.decompose.DefaultComponentContext
-import com.arkivanov.decompose.ExperimentalDecomposeApi
-import com.arkivanov.decompose.extensions.compose.jetbrains.lifecycle.LifecycleController
-import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 
-@ExperimentalDecomposeApi
 public fun main(args: Array<String>) {
     val argParser = ArgParser("Playground")
     val routerArgOption by argParser.option(ArgType.String, "route")
-
-    val lifecycleRegistry = LifecycleRegistry()
     val argResult = argParser.parse(args)
 
-    val circuitConfig = CircuitConfig(DefaultComponentContext(lifecycleRegistry))
     val initialBackStack = buildInitialBackStack(routerArgOption)
+    val circuitConfig = CircuitConfig()
 
     application {
         val windowState = rememberWindowState(size = DpSize(450.dp, 975.dp))
@@ -31,11 +24,6 @@ public fun main(args: Array<String>) {
             title = argResult.commandName,
             state = windowState,
         ) {
-            LifecycleController(
-                lifecycleRegistry = lifecycleRegistry,
-                windowState = windowState,
-            )
-
             LauncherApp(
                 initialBackStack = initialBackStack,
                 circuitConfig = circuitConfig,
