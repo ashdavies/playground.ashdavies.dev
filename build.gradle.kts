748322dc2c55ccbff7f284dd8a58a9f30067f797
Davies Ashley <ashdavies@users.noreply.github.com>
2023-02-13 23:43:24 +0100

    Update terraform scripts for iam member permission

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ plugins {
     alias(libs.plugins.benManes.versions)
     alias(libs.plugins.catalog.update)
     alias(libs.plugins.diffplug.spotless)
-    alias(libs.plugins.google.build.cache)
     alias(libs.plugins.gradle.doctor)
 
     // alias(libs.plugins.cash.molecule)
@@ -87,16 +86,5 @@ tasks.withType<DependencyUpdatesTask> {
 
     rejectVersionIf {
         isUnstable(candidate.version)
-    }
-}
-
-buildCache {
-    registerBuildCacheService(GcpBuildCache, GcpBuildCacheServiceFactory)
-
-    remote(GcpBuildCache) {
-        credentials = new ExportedKeyGcpCredentials(new File("path/to/credentials.json"))
-        bucketName = "gradle-build-cache"
-        projectId = "playground-1a136"
-        push = inCi
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,32 @@
+import androidx.build.gradle.gcpbuildcache.GcpBuildCache
+import androidx.build.gradle.gcpbuildcache.GcpBuildCacheServiceFactory
+
 apply(from = "repositories.gradle.kts")
 
+plugins {
+    id("androidx.build.gradle.gcpbuildcache") version "1.0.0-beta01"
+    id("com.gradle.enterprise") version "3.9"
+}
+
+buildCache {
+    registerBuildCacheService(GcpBuildCache::class, GcpBuildCacheServiceFactory::class)
+
+    remote(GcpBuildCache::class) {
+        bucketName = "playground-build-cache"
+        projectId = "playground-1a136"
+        isPush = true
+    }
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
 
 include(
     ":app-check:app-check-client",
@@ -39,18 +65,6 @@ includeBuild("compose-constructor/plugin-gradle") {
     dependencySubstitution {
         substitute(module("io.ashdavies.playground:plugin-gradle")).using(project(":"))
     }
-}
-
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishAlways()
-    }
-}
-
-plugins {
-    id("com.gradle.enterprise") version "3.9"
 }
 
 rootProject.name = "playground"
