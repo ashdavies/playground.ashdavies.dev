18577789713c1e6a0b223ea886ece02571f81e37
Davies Ashley <ashdavies@users.noreply.github.com>
2023-02-15 15:32:48 +0100

    Remove compose constructor module

--- compose-constructor/plugin-gradle/settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-apply(from = "../../repositories.gradle.kts")
-
-dependencyResolutionManagement {
-    versionCatalogs.create("libs") {
-        from(files("../../gradle/libs.versions.toml"))
-    }
-}
--- compose-constructor/plugin-runtime/src/commonMain/kotlin/io/ashdavies/playground/compose/Annotations.kt
+++
@@ -1,9 +0,0 @@
-package io.ashdavies.playground.compose
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.BINARY)
-public annotation class Provides
-
-@Target(AnnotationTarget.CONSTRUCTOR)
-@Retention(AnnotationRetention.BINARY)
-public annotation class Remember
