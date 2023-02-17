975ce7df42088bd75bf9e7dd9f3131ead5460eb9
Davies Ashley <ashdavies@users.noreply.github.com>
2023-02-17 17:52:23 +0100

    Resource cannot be extracted more simply

--- google-cloud/src/main/terraform/main.tf
+++ google-cloud/src/main/terraform/main.tf
@@ -150,30 +150,6 @@ resource "google_apikeys_key" "integration" {
   }
 }
 
-resource "google_project_iam_custom_role" "main" {
-  description = "Can create, update, and delete services necessary for the automatic deployment"
-  title       = "GitHub Actions Publisher"
-  role_id     = "actionsPublisher"
-  permissions = [
-    "artifactregistry.repositories.uploadArtifacts",
-    "cloudbuild.builds.create",
-    "iam.serviceAccounts.actAs",
-    "iam.serviceAccounts.getAccessToken",
-    "iam.serviceAccounts.signBlob",
-    "resourcemanager.projects.get",
-    "run.services.get",
-    "run.services.getIamPolicy",
-    "run.services.update",
-    "servicemanagement.services.get",
-    "servicemanagement.services.update",
-    "serviceusage.services.list",
-    "storage.buckets.list",
-    "storage.objects.create",
-    "storage.objects.delete",
-    "storage.objects.list",
-  ]
-}
-
 resource "google_project_service" "apikeys" {
   service = "apikeys.googleapis.com"
   project = var.project_id
