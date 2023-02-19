77a902a5f70ccd8cd83091b953d6292af05245c0
Davies Ashley <ashdavies@users.noreply.github.com>
2023-02-19 13:10:56 +0100

    Collapse back in resource

--- google-cloud/src/main/terraform/main.tf
+++ google-cloud/src/main/terraform/main.tf
@@ -24,3 +24,27 @@ resource "github_actions_secret" "google_workload_identity" {
   secret_name     = "google_workload_identity"
   repository      = var.gh_repo_name
 }
+
+resource "google_project_iam_custom_role" "main" {
+  description = "Can create, update, and delete services necessary for the automatic deployment"
+  title       = "GitHub Actions Publisher"
+  role_id     = "actionsPublisher"
+  permissions = [
+    "artifactregistry.repositories.uploadArtifacts",
+    "cloudbuild.builds.create",
+    "iam.serviceAccounts.actAs",
+    "iam.serviceAccounts.getAccessToken",
+    "iam.serviceAccounts.signBlob",
+    "resourcemanager.projects.get",
+    "run.services.get",
+    "run.services.getIamPolicy",
+    "run.services.update",
+    "servicemanagement.services.get",
+    "servicemanagement.services.update",
+    "serviceusage.services.list",
+    "storage.buckets.list",
+    "storage.objects.create",
+    "storage.objects.delete",
+    "storage.objects.list",
+  ]
+}
