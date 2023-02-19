8dc38aa9a7e0368f37cb1dd9ee99ff0ab7a53e13
Davies Ashley <ashdavies@users.noreply.github.com>
2023-02-19 13:10:19 +0100

    Remove secrets unobtainable by terraform

--- google-cloud/src/main/terraform/main.tf
+++ google-cloud/src/main/terraform/main.tf
@@ -19,20 +19,8 @@ resource "github_actions_secret" "google_service_account_id" {
   repository      = var.gh_repo_name
 }
 
-/*resource "github_actions_secret" "google_services_passphrase" {
-  plaintext_value = var.google_services_passphrase
-  secret_name     = "google_services_passphrase"
-  repository      = var.gh_repo_name
-}*/
-
 resource "github_actions_secret" "google_workload_identity" {
   plaintext_value = module.github-workload-identity.provider_name
   secret_name     = "google_workload_identity"
   repository      = var.gh_repo_name
 }
-
-/*resource "github_actions_secret" "mobile_sdk_app_id" {
-  plaintext_value = var.mobile_sdk_app_id
-  secret_name     = "mobile_sdk_app_id"
-  repository      = var.gh_repo_name
-}*/
