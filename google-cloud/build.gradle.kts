5da14bae904732a35f54f170c6541d26f6f57c3b
Davies Ashley <ashdavies@users.noreply.github.com>
2023-02-21 12:36:05 +0100

    Remove GitHub token providers

--- google-cloud/build.gradle.kts
+++ google-cloud/build.gradle.kts
@@ -1,11 +1,3 @@
-import org.jetbrains.gradle.plugins.terraform.tasks.AbstractTerraformExec
-
 plugins {
     id("io.ashdavies.terraform")
-}
-
-terraform {
-    sourceSets.main {
-        planVariables = mapOf("gh_token" to System.getenv("GITHUB_TOKEN"))
-    }
 }
--- google-cloud/src/main/terraform/providers.tf
+++ google-cloud/src/main/terraform/providers.tf
@@ -17,7 +17,6 @@ provider "docker" {
 }
 
 provider "github" {
-  token  = var.gh_token
   owner  = var.gh_owner
 }
 
