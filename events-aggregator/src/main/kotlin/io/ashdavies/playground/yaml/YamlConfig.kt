80401d496ebe758e8e3ade99d931b3b7754dbe3c
Davies Ashley <ashdavies@users.noreply.github.com>
2023-03-17 22:50:21 +0100

    Extract yaml serialization to new module

--- events-aggregator/src/main/kotlin/io/ashdavies/playground/yaml/YamlConfig.kt
+++
@@ -1,8 +0,0 @@
-package io.ashdavies.playground.yaml
-
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.modules.EmptySerializersModule
-import kotlinx.serialization.modules.SerializersModule
-
-@ExperimentalSerializationApi
-internal data class YamlConfig(val serializersModule: SerializersModule = EmptySerializersModule)
--- micro-yaml/src/commonMain/kotlin/io/ashdavies/yaml/Yaml.kt
+++ micro-yaml/src/commonMain/kotlin/io/ashdavies/yaml/Yaml.kt
@@ -1,9 +1,13 @@
-package io.ashdavies.playground.yaml
+package io.ashdavies.yaml
 
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.StringFormat
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.AbstractDecoder
 import kotlinx.serialization.encoding.CompositeDecoder
+import kotlinx.serialization.modules.EmptySerializersModule
 import kotlinx.serialization.modules.SerializersModule
 
 private const val TOKEN_COLON = ":"
@@ -12,7 +16,31 @@ private const val TOKEN_DOCUMENT = "---"
 private const val TOKEN_QUOTE = "\""
 
 @ExperimentalSerializationApi
-internal class YamlDecoder(override val serializersModule: SerializersModule, string: String) : AbstractDecoder() {
+public sealed class Yaml(configuration: YamlConfig) : StringFormat {
+
+    override val serializersModule: SerializersModule = configuration.serializersModule
+
+    override fun <T> encodeToString(serializer: SerializationStrategy<T>, value: T): String {
+        throw UnsupportedOperationException("Yaml serialization is not supported")
+    }
+
+    override fun <T> decodeFromString(deserializer: DeserializationStrategy<T>, string: String): T {
+        return deserializer.deserialize(YamlDecoder(serializersModule, string))
+    }
+
+    public companion object Default : Yaml(YamlConfig())
+}
+
+@ExperimentalSerializationApi
+public data class YamlConfig(
+    val serializersModule: SerializersModule = EmptySerializersModule()
+)
+
+@ExperimentalSerializationApi
+internal class YamlDecoder(
+    override val serializersModule: SerializersModule,
+    string: String,
+) : AbstractDecoder() {
 
     private val lineSequence: Iterator<String> = string
         .lineSequence()
@@ -20,7 +48,9 @@ internal class YamlDecoder(override val serializersModule: SerializersModule, st
 
     private lateinit var currentToken: String
 
-    override fun decodeElementIndex(descriptor: SerialDescriptor): Int = when (lineSequence.hasNext()) {
+    override fun decodeElementIndex(
+        descriptor: SerialDescriptor,
+    ): Int = when (lineSequence.hasNext()) {
         true -> descriptor.getElementIndex(decodeKey())
         false -> CompositeDecoder.DECODE_DONE
     }
@@ -55,5 +85,7 @@ internal class YamlDecoder(override val serializersModule: SerializersModule, st
         } while (!hasValidToken() && lineSequence.hasNext())
     }
 
-    private fun hasValidToken(): Boolean = currentToken != TOKEN_DOCUMENT && currentToken.isNotEmpty()
+    private fun hasValidToken(): Boolean {
+        return currentToken != TOKEN_DOCUMENT && currentToken.isNotEmpty()
+    }
 }
