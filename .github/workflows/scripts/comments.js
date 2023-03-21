3c540cfe84b12ffd1513bf10a871274062e12336
Davies Ashley <ashdavies@users.noreply.github.com>
2023-03-21 18:36:04 +0100

    Extract comments function

---
+++ .github/workflows/scripts/comments.js
@@ -0,0 +1,28 @@
+exports.get = async function (context, github) {
+  const comments = await github.rest.issues.listComments({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+
+  return comments.data.find(comment =>
+   comment.user.login.endsWith('[bot]') && comment.user.type === 'Bot'
+  );
+};
+
+exports.create = function (context, github, body) {
+  return github.rest.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body,
+  });
+};
+
+exports.delete = function (context, github, id) {
+  return github.rest.issues.deleteComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    comment_id: id,
+  });
+};
