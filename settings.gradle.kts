87a8154f21d36731eafb2c2a97caeb166a6474c9
Playground Manager <126197455+playground-manager[bot]@users.noreply.github.com>
2023-03-21 08:10:35 +0000

    chore(deps): update plugin com.gradle.enterprise to v3.12.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ apply(from = "repositories.gradle.kts")
 plugins {
     id("androidx.build.gradle.gcpbuildcache") version "1.0.0-beta01"
     id("com.google.cloud.tools.jib") version "3.3.1" apply false
-    id("com.gradle.enterprise") version "3.12.4"
+    id("com.gradle.enterprise") version "3.12.5"
 }
 
 buildCache {
