e42726f2985b7f3235533f18a2883d838a5bcf6b
Davies Ashley <ashdavies@users.noreply.github.com>
2023-03-23 17:05:41 +0100

    Include home screen previews

--- events-app/src/androidDebug/kotlin/io/ashdavies/playground/home/HomeScreen.android.kt
+++ events-app/src/androidDebug/kotlin/io/ashdavies/playground/home/HomeScreen.android.kt
@@ -5,13 +5,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.paging.compose.collectAsLazyPagingItems
 import app.cash.paging.PagingData
+import io.ashdavies.playground.AndroidMakers
+import io.ashdavies.playground.DroidconBerlin
+import io.ashdavies.playground.DroidconLondon
 import io.ashdavies.playground.Event
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 
 @Preview
 @Composable
+private fun HomeScreen(items: List<Event> = listOf(AndroidMakers, DroidconBerlin, DroidconLondon)) {
+    HomeScreen(flowOf(PagingData.from(items)))
+}
+
+@Composable
 @OptIn(ExperimentalMaterial3Api::class)
-private fun HomeScreen(pagingData: Flow<PagingData<Event>> = emptyFlow()) {
+private fun HomeScreen(pagingData: Flow<PagingData<Event>>) {
     HomeScreen(HomeScreen.State(pagingData.collectAsLazyPagingItems()) { })
 }
