e533aa74630eebcaae9f1426f387f4e20902ce88
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2023-04-14 16:29:32 +0200

    Inline function calls for launcher (#237)
    * Inline function calls for launcher
    
    * Apply spotless checks
    
    ---------
    
    Co-authored-by: Davies Ashley <ashdavies@users.noreply.github.com>

--- app-launcher/android/src/main/kotlin/io/ashdavies/playground/LauncherActivity.kt
+++ app-launcher/android/src/main/kotlin/io/ashdavies/playground/LauncherActivity.kt
@@ -1,15 +1,36 @@
 package io.ashdavies.playground
 
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.slack.circuit.CircuitCompositionLocals
+import com.slack.circuit.NavigableCircuitContent
+import com.slack.circuit.backstack.rememberSaveableBackStack
+import com.slack.circuit.overlay.ContentWithOverlays
+import com.slack.circuit.push
+import com.slack.circuit.rememberCircuitNavigator
+import io.ashdavies.compose.ProvideFirebaseApp
 
 internal class LauncherActivity : KotlinActivity({
     val initialBackStack = buildInitialBackStack(intent.getStringExtra("route"))
     val circuitConfig = CircuitConfig()
 
     setContent {
-        LauncherApp(
-            initialBackStack = initialBackStack,
-            circuitConfig = circuitConfig,
-        )
+        val systemUiController = rememberSystemUiController()
+        val colorScheme = dynamicColorScheme()
+
+        val primaryContainer = colorScheme.primaryContainer
+        systemUiController.setSystemBarsColor(primaryContainer)
+
+        val backStack = rememberSaveableBackStack { initialBackStack.forEach(::push) }
+        val navigator = rememberCircuitNavigator(backStack)
+
+        ProvideFirebaseApp {
+            MaterialTheme(colorScheme = colorScheme) {
+                CircuitCompositionLocals(circuitConfig) {
+                    ContentWithOverlays { NavigableCircuitContent(navigator, backStack) }
+                }
+            }
+        }
     }
 },)
