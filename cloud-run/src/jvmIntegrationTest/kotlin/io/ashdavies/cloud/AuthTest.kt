8aaabc7dc5361b97f0a2b169519a4c43443f6052
Davies Ashley <ashdavies@users.noreply.github.com>
2023-05-05 22:39:57 +0200

    Update remaining references

--- cloud-run/src/jvmIntegrationTest/kotlin/io/ashdavies/cloud/AuthTest.kt
+++ cloud-run/src/jvmIntegrationTest/kotlin/io/ashdavies/cloud/AuthTest.kt
@@ -1,14 +1,11 @@
 package io.ashdavies.cloud
 
 import io.ktor.client.call.body
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
-import io.ktor.serialization.kotlinx.json.json
-import io.ktor.server.testing.testApplication
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlin.test.Test
 import kotlin.test.assertNotNull
@@ -17,8 +14,7 @@ import kotlin.test.assertNotNull
 internal class AuthTest {
 
     @Test
-    fun `should sign in with custom token`() = testApplication {
-        val client = createClient { install(ContentNegotiation, ContentNegotiation.Config::json) }
+    fun `should sign in with custom token`() = testMainApplication { client ->
         val apiKey = requireNotNull(System.getenv("GOOGLE_PROJECT_API_KEY"))
 
         val authResult = client.post("/firebase/auth") {
--- cloud-run/src/jvmIntegrationTest/kotlin/io/ashdavies/cloud/HelloTest.kt
+++ cloud-run/src/jvmIntegrationTest/kotlin/io/ashdavies/cloud/HelloTest.kt
@@ -3,15 +3,13 @@ package io.ashdavies.cloud
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.testing.testApplication
 import kotlin.test.Test
-import kotlin.test.fail
 import kotlin.test.assertEquals
 
 internal class HelloTest {
 
     @Test
-    fun `should create test application`() = testApplication {
+    fun `should create test application`() = testMainApplication { client ->
         val response = client.get("/hello")
 
         assertEquals(
@@ -23,10 +21,5 @@ internal class HelloTest {
             actual = response.bodyAsText(),
             expected = "Hello, World!",
         )
-    }
-
-    @Test
-    fun `should fail`() = {
-        fail()
     }
 }
