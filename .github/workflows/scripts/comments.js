a71a97df102e89c8a301f1de9f49df8d66ca6163
Davies Ashley <ashdavies@users.noreply.github.com>
2023-05-11 15:38:42 +0200

    Include javascript in spotless configuration

--- .github/workflows/scripts/comments.js
+++ .github/workflows/scripts/comments.js
@@ -1,15 +1,15 @@
-exports.findAll = async function (context, github, predicate = it => true) {
+exports.findAll = async function (context, github, predicate = (it) => true) {
   const comments = await github.rest.issues.listComments({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
   });
 
-  return comments.data.filter(comment =>
-    comment.user.login.endsWith('[bot]')
-    && comment.user.type === 'Bot'
-    && predicate(comment)
-  );
+  return comments.data.filter((comment) => {
+    comment.user.login.endsWith("[bot]") &&
+      comment.user.type === "Bot" &&
+      predicate(comment);
+  });
 };
 
 exports.create = function (context, github, body) {
@@ -29,8 +29,8 @@ exports.delete = function (context, github, id) {
   });
 };
 
-exports.deleteAll = function (context, github, predicate = it => true) {
+exports.deleteAll = function (context, github, predicate = (it) => true) {
   for (const item in exports.findAll(context, github, predicate)) {
-    delete (context, github, item.id)
+    delete (context, github, item.id);
   }
 };
