00deaca641c8b5d914072ff5f2e6b360e6c0bba6
Davies Ashley <ashdavies@users.noreply.github.com>
2023-05-16 17:45:03 +0200

    Drop Terraform show

--- terraform.sh
+++ terraform.sh
@@ -1,8 +1,5 @@
-# bash << EOF | tee terraform.log
 bash << EOF > terraform.log
 ./gradlew terraformPlan \
   --no-configuration-cache \
-  --console=plain \
-  --dry-run \
-  --quiet
+  --console=plain
 EOF
