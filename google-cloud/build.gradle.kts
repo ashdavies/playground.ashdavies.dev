b41c22fe195574387caa5f10bb6c57b23de78fc5
Davies Ashley <ashdavies@users.noreply.github.com>
2023-05-17 14:44:12 +0200

    Create custom gradle task to generate human readable output

--- google-cloud/build.gradle.kts
+++ google-cloud/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jetbrains.gradle.plugins.terraform.TerraformPlugin
+import org.jetbrains.gradle.plugins.terraform.tasks.AbstractTerraformExec
+import org.jetbrains.gradle.plugins.terraform.tasks.TerraformPlan
+
 plugins {
     id("io.ashdavies.terraform")
 }
@@ -5,8 +9,34 @@ plugins {
 terraform {
     showPlanOutputInConsole = false
     showInitOutputInConsole = false
-    
-    sourceSets.main {
+
+    val main by sourceSets.getting {
         planVariables = mapOf("gh_token" to System.getenv("GITHUB_TOKEN"))
+    }
+
+    val terraformShowText by tasks.registering(TerraformShowText::class) {
+        outputTextPlanFile = project.file("${main.baseBuildDir}/plan.txt")
+        sourcesDirectory = main.runtimeExecutionDirectory
+        dependsOn(tasks.withType<TerraformPlan>())
+        inputPlanFile = main.outputBinaryPlan
+        group = TerraformPlugin.TASK_GROUP
+        dataDir = main.dataDir
+    }
+}
+
+abstract class TerraformShowText : AbstractTerraformExec() {
+
+    @get:InputFile
+    var inputPlanFile by project.objects.property<File>()
+
+    @get:OutputFile
+    var outputTextPlanFile by project.objects.property<File>()
+
+    override fun ExecSpec.customizeExec() {
+        standardOutput = outputTextPlanFile.outputStream()
+    }
+
+    override fun getTerraformArguments(): List<String> {
+        return listOf("show", "-no-color", inputPlanFile.absolutePath)
     }
 }
