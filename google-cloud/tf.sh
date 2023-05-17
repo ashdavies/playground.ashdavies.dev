5646e85a6e46cc366a9f37adc569109392fb2751
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2023-05-17 16:18:44 +0200

    Terraform Plan Output (#289)
    * Create custom gradle task to generate human readable output
    
    * Revise body path
    
    * Provide empty string for false case
    
    * Encapsulate body in code block
    
    * Escape backticks and remove unnecessary expressions
    
    * Remove new lines
    
    * Roll back pre-merge workflow
    
    ---------
    
    Co-authored-by: Davies Ashley <ashdavies@users.noreply.github.com>

--- google-cloud/tf.sh
+++ google-cloud/tf.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+TF_VAR_gh_token=$(gh auth token)
+DIR=$(dirname -- "$0")
+PATH=$(realpath $DIR)
 
-DIR="$(dirname -- "$0")"
+/bin/ln -sf ${PATH}/build/terraform/main/data ${PATH}/build/terraform/main/runtimeExecution/.terraform
 $DIR/build/terraform/terraform* -chdir=$DIR/build/terraform/main/runtimeExecution $@
+ 
--- terraform.sh
+++
@@ -1,5 +0,0 @@
-bash << EOF > terraform.log
-./gradlew terraformPlan \
-  --no-configuration-cache \
-  --console=plain
-EOF
