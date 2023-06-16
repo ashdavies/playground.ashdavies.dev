dfef8bc39349b9219d31e6ce1aa1ae506bd95ad5
Davies Ashley <ashdavies@users.noreply.github.com>
2023-06-16 16:08:22 +0200

    Disable configuration cache for spotless task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress("DSL_SCOPE_VIOLATION") // https://github.com/gradle/gradle/issues/22797
 
 import com.diffplug.gradle.spotless.FormatExtension
+import com.diffplug.gradle.spotless.SpotlessTask
 import com.diffplug.spotless.LineEnding
 
 buildscript {
@@ -85,4 +86,8 @@ spotless {
 
     lineEndings = LineEnding.PLATFORM_NATIVE
     ratchetFrom = "origin/main"
+}
+
+tasks.withType<SpotlessTask> {
+    notCompatibleWithConfigurationCache("https://github.com/diffplug/spotless/issues/1644")
 }
