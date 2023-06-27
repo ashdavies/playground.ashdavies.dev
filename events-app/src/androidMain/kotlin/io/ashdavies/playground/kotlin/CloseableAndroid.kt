313f1070c98ab401531adaa60f6ee1e2afe3b42d
Davies Ashley <ashdavies@users.noreply.github.com>
2023-06-27 13:52:37 +0200

    Drop multiplatform Closable

--- events-app/src/androidMain/kotlin/io/ashdavies/playground/kotlin/CloseableAndroid.kt
+++
@@ -1,7 +0,0 @@
-package io.ashdavies.playground.kotlin
-
-public actual typealias Closeable = java.io.Closeable
-
-public actual fun Closeable(close: () -> Unit): Closeable {
-    return java.io.Closeable(close)
-}
