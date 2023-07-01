dc615a6f315f610192726d5b89f9b005d6cd3533
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2023-07-01 08:50:01 +0200

    Apply spotless to all subprojects (#349)
    Co-authored-by: Davies Ashley <ashdavies@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress("DSL_SCOPE_VIOLATION") // https://github.com/gradle/gradle/issues/22797
 
 import com.diffplug.gradle.spotless.FormatExtension
+import com.diffplug.gradle.spotless.SpotlessExtension
 
 buildscript {
     dependencies {
@@ -16,6 +17,7 @@ plugins {
         classpath(android.application)
         classpath(android.library)
         classpath(apollo.graphql)
+        classpath(diffplug.spotless)
         classpath(google.services)
         classpath(kotlinx.kover)
 
@@ -26,7 +28,6 @@ plugins {
         }
 
         alias(dependency.analysis)
-        alias(diffplug.spotless)
         alias(gradle.doctor)
     }
 }
@@ -37,53 +38,61 @@ doctor {
     javaHome { failOnError.set(false) }
 }
 
-spotless {
-    javascript {
-        target("**/*.js")
-        prettier()
+subprojects {
+    fun apply(plugin: PluginDependency) = plugins.apply(plugin.pluginId)
+
+    with(rootProject.libs.plugins) {
+        apply(plugin = diffplug.spotless.get())
     }
 
-    val ktLintVersion = libs.versions.pinterest.ktlint.get()
-    fun FormatExtension.kotlinDefault(extension: String = "kt") {
-        target("**/src/**/*.$extension")
-        targetExclude("**/build/**")
-        trimTrailingWhitespace()
-        endWithNewline()
+    configure<SpotlessExtension> {
+        javascript {
+            target("**/*.js")
+            prettier()
+        }
+
+        val ktLintVersion = rootProject.libs.versions.pinterest.ktlint.get()
+        fun FormatExtension.kotlinDefault(extension: String = "kt") {
+            target("**/src/**/*.$extension")
+            targetExclude("**/build/**")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+
+        kotlinGradle {
+            kotlinDefault("gradle.kts")
+            ktlint(ktLintVersion)
+        }
+
+        kotlin {
+            val editorConfig = mapOf(
+                "ij_kotlin_allow_trailing_comma_on_call_site" to "true",
+                "ij_kotlin_allow_trailing_comma" to "true",
+                "disabled_rules" to "filename",
+                "experimental" to "true",
+                "android" to "true",
+            )
+
+            ktlint(ktLintVersion)
+                .editorConfigOverride(editorConfig)
+                .setUseExperimental(true)
+
+            kotlinDefault()
+        }
+
+        format("misc") {
+            target(".gitignore", "*.md", "*.toml")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+
+        val terraformPath = System.getenv("TERRAFORM_PATH")
+        if (terraformPath != null) format("terraform") {
+            val terraformExe = "$terraformPath/terraform_1.3.1"
+            nativeCmd("terraform", terraformExe, listOf("fmt", "-"))
+            target("src/main/terraform/**/*.tf")
+        }
+
+        ratchetFrom = "origin/main"
     }
-
-    kotlinGradle {
-        kotlinDefault("gradle.kts")
-        ktlint(ktLintVersion)
-    }
-
-    kotlin {
-        val editorConfig = mapOf(
-            "ij_kotlin_allow_trailing_comma_on_call_site" to "true",
-            "ij_kotlin_allow_trailing_comma" to "true",
-            "disabled_rules" to "filename",
-            "experimental" to "true",
-            "android" to "true",
-        )
-
-        ktlint(ktLintVersion)
-            .editorConfigOverride(editorConfig)
-            .setUseExperimental(true)
-
-        kotlinDefault()
-    }
-
-    format("misc") {
-        target(".gitignore", "*.md", "*.toml")
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
-
-    val terraformPath = System.getenv("TERRAFORM_PATH")
-    if (terraformPath != null) format("terraform") {
-        val terraformExe = "$terraformPath/terraform_1.3.1"
-        nativeCmd("terraform", terraformExe, listOf("fmt", "-"))
-        target("src/main/terraform/**/*.tf")
-    }
-
-    ratchetFrom = "origin/main"
 }
