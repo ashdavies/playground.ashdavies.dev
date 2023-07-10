dad6b0f2142273b550fa856930e0aecfe7b72b18
Davies Ashley <ashdavies@users.noreply.github.com>
2023-07-10 14:08:31 +0200

    Create initial gallery app module with app launcher integration

---
+++ gallery-app/src/commonMain/kotlin/io/ashdavies/gallery/GalleryPresenter.kt
@@ -0,0 +1,50 @@
+package io.ashdavies.gallery
+
+import androidx.compose.runtime.Composable
+import com.arkivanov.essenty.parcelable.Parcelable
+import com.arkivanov.essenty.parcelable.Parcelize
+import com.slack.circuit.runtime.*
+import com.slack.circuit.runtime.presenter.Presenter
+import com.slack.circuit.runtime.presenter.presenterOf
+import com.slack.circuit.runtime.ui.Ui
+import com.slack.circuit.runtime.ui.ui
+
+@Parcelize
+public object GalleryScreen : Parcelable, Screen {
+
+    internal sealed interface Event : CircuitUiEvent {
+        object Pop : Event
+    }
+
+    internal data class State(
+        val eventSink: (Event) -> Unit,
+    ) : CircuitUiState
+}
+
+public class GalleryPresenterFactory : Presenter.Factory {
+    override fun create(
+        screen: Screen,
+        navigator: Navigator,
+        context: CircuitContext,
+    ): Presenter<*>? = when (screen) {
+        is GalleryScreen -> presenterOf { GalleryPresenter(navigator) }
+        else -> null
+    }
+}
+
+public class GalleryUiFactory : Ui.Factory {
+    override fun create(screen: Screen, context: CircuitContext): Ui<*>? = when (screen) {
+        is GalleryScreen -> ui<GalleryScreen.State> { state, modifier ->
+            GalleryScreen(state, modifier)
+        }
+
+        else -> null
+    }
+}
+
+@Composable
+internal fun GalleryPresenter(
+    navigator: Navigator,
+) = GalleryScreen.State {
+
+}
