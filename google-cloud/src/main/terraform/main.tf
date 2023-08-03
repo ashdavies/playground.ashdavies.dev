2aa1f8a8731ba59ab6ea04019fbde3ebeddb212a
Ash Davies <ashdavies@users.noreply.github.com>
2023-08-03 16:42:23 +0200

    chore(terraform): remove deprecated code related to github_branch.main resource chore(terraform): remove unused outputs.tf file in github/repository module fix(openapi-v2.yml): remove unused x-version-name property in info section

--- google-cloud/src/main/terraform/main.tf
+++ google-cloud/src/main/terraform/main.tf
@@ -36,7 +36,6 @@ resource "google_project_iam_custom_role" "main" {
 
 resource "local_file" "openapi_config" {
   content  = templatefile(var.openapi_config, {
-    version_name         = substr(module.github-repository.sha, 0, 7)
     backend_service_name = module.cloud-run-build.url
   })
   filename = basename(var.openapi_config)
