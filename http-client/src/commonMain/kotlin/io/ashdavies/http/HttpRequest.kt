76c44aee33c8b5253c019fa09f3bf0203f3e2179
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2023-08-17 21:04:08 +0200

    Remove unused methods

--- http-client/src/commonMain/kotlin/io/ashdavies/http/HttpRequest.kt
+++ http-client/src/commonMain/kotlin/io/ashdavies/http/HttpRequest.kt
@@ -1,9 +1,7 @@
 package io.ashdavies.http
 
-import io.ktor.client.plugins.onDownload
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.parameter
-import io.ktor.http.path
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.properties.Properties
 import kotlinx.serialization.properties.encodeToMap
@@ -11,15 +9,4 @@ import kotlinx.serialization.properties.encodeToMap
 @OptIn(ExperimentalSerializationApi::class)
 public inline fun <reified T> HttpRequestBuilder.parameter(value: T) {
     Properties.encodeToMap(value).forEach { (key, value) -> parameter(key, value) }
-}
-
-public fun HttpRequestBuilder.path(vararg path: String) {
-    url.path(*path)
-}
-
-@PublishedApi
-internal fun HttpRequestBuilder.onProgress(listener: suspend (Float) -> Unit) {
-    onDownload { bytesSentTotal, contentLength ->
-        listener((bytesSentTotal.toFloat() / contentLength).coerceAtMost(1.0F))
-    }
 }
--- http-client/src/commonMain/kotlin/io/ashdavies/http/State.kt
+++ http-client/src/commonMain/kotlin/io/ashdavies/http/State.kt
@@ -1,7 +1,6 @@
 package io.ashdavies.http
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ProduceStateScope
 import androidx.compose.runtime.State
 import androidx.compose.runtime.produceState
 import kotlinx.coroutines.CoroutineScope
@@ -9,9 +8,4 @@ import kotlinx.coroutines.CoroutineScope
 @Composable
 public fun <T : Any> produceStateInline(block: suspend CoroutineScope.() -> T): State<Result<T>> {
     return produceState(Result.loading()) { value = runCatching { block() } }
-}
-
-@Composable
-public fun <T : Any> produceState(producer: suspend ProduceStateScope<Result<T>>.() -> Unit): State<Result<T>> {
-    return produceState(Result.loading(), producer)
 }
