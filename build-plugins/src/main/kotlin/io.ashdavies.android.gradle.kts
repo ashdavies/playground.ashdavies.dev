391d53ed0cb623a7f0e166084d693ac2db870efc
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2023-12-06 23:50:35 +0100

    Reset compiler flags, drop custom version accessor (#683)

--- build-plugins/src/main/kotlin/io.ashdavies.android.gradle.kts
+++ build-plugins/src/main/kotlin/io.ashdavies.android.gradle.kts
@@ -30,13 +30,17 @@ fun CommonExtension<*, *, *, *, *>.configure() {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        val jvmTargetVersion = libs.versions.kotlin.jvmTarget.get()
+        sourceCompatibility(jvmTargetVersion)
+        targetCompatibility(jvmTargetVersion)
     }
 
     defaultConfig {
-        compileSdk = Playground.compileSdk
-        minSdk = Playground.minSdk
+        val compileSdkVersion = libs.versions.android.compileSdk.get()
+        compileSdk = compileSdkVersion.toInt()
+
+        val minSdkVersion = libs.versions.android.minSdk.get()
+        minSdk = minSdkVersion.toInt()
     }
 
     sourceSets.configureEach {
