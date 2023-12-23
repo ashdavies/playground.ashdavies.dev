96bd22248970f226696956b623e72149e0729301
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2023-12-23 12:34:57 +0100

    Specify kotlinx datetime as date library (#738)

--- http-client/build.gradle.kts
+++ http-client/build.gradle.kts
@@ -41,6 +41,7 @@ openApiGenerate {
     inputSpec.set("$rootDir/${property("openapi.generator.inputSpec")}")
     templateDir.set("$projectDir/src/commonMain/resources/templates")
     packageName.set("io.ashdavies.generated")
+    additionalProperties.put("dateLibrary", "kotlinx-datetime")
     serverVariables.put("cloud_run_hostname", String())
     ignoreFileOverride.set("$projectDir/.openapi-generator-ignore")
     configOptions.put("library", "multiplatform")
