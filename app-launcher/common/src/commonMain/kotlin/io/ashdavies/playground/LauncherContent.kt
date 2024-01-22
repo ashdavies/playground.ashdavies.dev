62c7ec7ef7892a34350252da3d6dba7eead42e18
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2024-01-22 20:34:38 +0100

    Remove network fetcher

--- app-launcher/common/src/commonMain/kotlin/io/ashdavies/playground/LauncherContent.kt
+++ app-launcher/common/src/commonMain/kotlin/io/ashdavies/playground/LauncherContent.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Composable
 import coil3.ImageLoader
 import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.setSingletonImageLoaderFactory
-import coil3.network.NetworkFetcher
 import io.ashdavies.check.ProvideAppCheckToken
 import io.ashdavies.material.dynamicColorScheme
 
@@ -13,8 +12,8 @@ import io.ashdavies.material.dynamicColorScheme
 @OptIn(ExperimentalCoilApi::class)
 public fun LauncherContent(content: @Composable () -> Unit) {
     setSingletonImageLoaderFactory { context ->
-        ImageLoader.Builder(context)
-            .components { add(NetworkFetcher.Factory()) }
+        ImageLoader
+            .Builder(context)
             .build()
     }
 
