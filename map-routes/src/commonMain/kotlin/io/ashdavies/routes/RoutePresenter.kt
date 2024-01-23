2dec6e50045e625cb1c690c7e0a11bce328995f8
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2024-01-23 19:21:02 +0100

    Include missing trailing comma

--- map-routes/src/commonMain/kotlin/io/ashdavies/routes/RoutePresenter.kt
+++ map-routes/src/commonMain/kotlin/io/ashdavies/routes/RoutePresenter.kt
@@ -23,7 +23,7 @@ internal fun RoutePresenter(locationService: LocationService): CircuitUiState {
     return RouteScreen.State(
         mapState = RouteMapState(
             startPosition = startPosition,
-            hasLocationPermission = locationPermissionState.allPermissionsGranted
+            hasLocationPermission = locationPermissionState.allPermissionsGranted,
         ),
     ) { }
 }
