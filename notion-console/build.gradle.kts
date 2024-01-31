08ba41ec99824fb59bd79b6f13ad3d8768562e44
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2024-01-31 13:34:27 +0100

    Merge Notion modules

--- notion-console/build.gradle.kts
+++ notion-console/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmCompilation
 plugins {
     id("io.ashdavies.compose")
     id("io.ashdavies.kotlin")
+    id("io.ashdavies.sql")
 
     application
 }
@@ -15,13 +16,24 @@ kotlin {
     commonMain.dependencies {
         implementation(projects.composeCli)
         implementation(projects.localStorage)
-        implementation(projects.notionClient)
 
         implementation(compose.runtime)
         implementation(compose.ui)
 
         implementation(libs.jraf.klibnotion)
         implementation(libs.kotlinx.cli)
+        implementation(libs.kotlinx.serialization.core)
+        implementation(libs.kotlinx.serialization.json)
+        implementation(libs.ktor.client.auth)
+        implementation(libs.ktor.client.content.negotiation)
+        implementation(libs.ktor.client.core)
+        implementation(libs.ktor.client.logging)
+        implementation(libs.ktor.http)
+        implementation(libs.ktor.serialization.kotlinx.json)
+        implementation(libs.ktor.server.auth)
+        implementation(libs.ktor.server.cio)
+        implementation(libs.ktor.server.core)
+        implementation(libs.ktor.server.host.common)
         implementation(libs.mosaic.runtime)
     }
 
@@ -42,5 +54,14 @@ kotlin {
 configurations.all {
     resolutionStrategy {
         force(libs.fusesource.jansi)
+    }
+}
+
+sqldelight {
+    databases {
+        create("PlaygroundDatabase") {
+            packageName.set("io.ashdavies.notion")
+            dependency(projects.localStorage)
+        }
     }
 }
