05ddde9242215e27d646bb5a3a8c8c4e257f691f
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2024-02-02 12:36:36 +0100

    Remove android namespace

--- http-client/build.gradle.kts
+++ http-client/build.gradle.kts
@@ -6,10 +6,6 @@ plugins {
     alias(libs.plugins.openapi.generator)
 }
 
-android {
-    namespace = "io.ashdavies.http.client"
-}
-
 kotlin {
     commonMain.dependencies {
         implementation(projects.httpCommon)
