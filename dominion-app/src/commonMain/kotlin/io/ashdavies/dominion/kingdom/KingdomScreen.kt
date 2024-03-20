4aa8de62651ad29f17f524693f6ae91e1858f3c2
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2024-03-20 11:22:50 +0100

    Use auto mirrored for reversable image vectors (#910)

--- dominion-app/src/commonMain/kotlin/io/ashdavies/dominion/kingdom/KingdomScreen.kt
+++ dominion-app/src/commonMain/kotlin/io/ashdavies/dominion/kingdom/KingdomScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
@@ -115,7 +115,7 @@ private fun KingdomTopBar(
 private fun BackIconButton(onClick: () -> Unit) {
     IconButton(onClick = onClick) {
         Image(
-            painter = rememberVectorPainter(Icons.Filled.ArrowBack),
+            painter = rememberVectorPainter(Icons.AutoMirrored.Filled.ArrowBack),
             contentDescription = null,
         )
     }
