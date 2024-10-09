d3e3022b5cc913ad87d2d2c5387b377be8dce761
Ashley Davies <ashdavies@users.noreply.github.com>
2024-10-09 23:07:40 +0200

    Rename config

--- http-client/src/commonMain/kotlin/io/ashdavies/http/CallValidator.kt
+++ http-client/src/commonMain/kotlin/io/ashdavies/http/CallValidator.kt
@@ -2,9 +2,9 @@ package io.ashdavies.http
 
 import io.ktor.client.call.body
 import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.plugins.HttpCallValidator
+import io.ktor.client.plugins.HttpCallValidatorConfig
 
-public inline fun <reified T : Throwable> HttpCallValidator.Config.throwClientRequestExceptionAs() {
+public inline fun <reified T : Throwable> HttpCallValidatorConfig.throwClientRequestExceptionAs() {
     handleResponseExceptionWithRequest { exception, _ ->
         if (exception is ClientRequestException) throw exception.response.body<T>()
     }
