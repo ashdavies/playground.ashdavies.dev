2111d46922f842444574a38fd3bd043295788a16
Ashley Davies <ashdavies@users.noreply.github.com>
2024-10-23 23:56:34 +0200

    Employ compose adaptive for dominion

--- app-launcher/common/src/commonMain/kotlin/io/ashdavies/playground/LauncherContent.kt
+++ app-launcher/common/src/commonMain/kotlin/io/ashdavies/playground/LauncherContent.kt
@@ -3,11 +3,13 @@ package io.ashdavies.playground
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import app.cash.sqldelight.EnumColumnAdapter
+import coil3.EventListener
 import coil3.ImageLoader
-import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.setSingletonImageLoaderFactory
 import coil3.disk.DiskCache
 import coil3.memory.MemoryCache
+import coil3.request.ErrorResult
+import coil3.request.ImageRequest
 import io.ashdavies.check.ProvideAppCheckToken
 import io.ashdavies.common.PlaygroundDatabase
 import io.ashdavies.content.PlatformContext
@@ -23,10 +25,17 @@ private const val MAX_SIZE_BYTES = 250L * 1024 * 1024
 private const val MAX_SIZE_PERCENT = 0.2
 
 @Composable
-@OptIn(ExperimentalCoilApi::class)
 public fun LauncherContent(context: PlatformContext, content: @Composable () -> Unit) {
     setSingletonImageLoaderFactory { ctx ->
         ImageLoader.Builder(ctx)
+            .eventListener(
+                object : EventListener() {
+                    override fun onError(request: ImageRequest, result: ErrorResult) {
+                        println("Error loading image: ${request.data}: $result")
+                        result.throwable.printStackTrace()
+                    }
+                },
+            )
             .memoryCache {
                 MemoryCache.Builder()
                     .maxSizePercent(ctx, MAX_SIZE_PERCENT)
