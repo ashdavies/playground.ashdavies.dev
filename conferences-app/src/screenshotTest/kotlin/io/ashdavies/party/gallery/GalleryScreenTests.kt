5b4edd1cfa3590b73f7544e6398a49d469f87fc2
Ashley Davies <ashdavies@users.noreply.github.com>
2025-01-14 00:01:04 +0100

    List past events

--- conferences-app/src/screenshotTest/kotlin/io/ashdavies/party/gallery/GalleryScreenTests.kt
+++ conferences-app/src/screenshotTest/kotlin/io/ashdavies/party/gallery/GalleryScreenTests.kt
@@ -1,9 +1,6 @@
 package io.ashdavies.party.gallery
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
-import io.ashdavies.party.past.GalleryGrid
-import io.ashdavies.party.past.GalleryScreen
 import io.ashdavies.party.tooling.MaterialPreviewTheme
 import io.ashdavies.party.tooling.PreviewDayNight
 import kotlinx.collections.immutable.persistentListOf
@@ -12,7 +9,6 @@ internal class GalleryScreenTests {
 
     @Composable
     @PreviewDayNight
-    @OptIn(ExperimentalFoundationApi::class)
     private fun GalleryGridPreview() {
         MaterialPreviewTheme {
             GalleryGrid(
