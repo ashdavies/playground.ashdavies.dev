f246f682d2f7132907c6f381aebab4f38375e7e0
Ashley Davies <ashdavies@users.noreply.github.com>
2025-01-19 22:34:27 +0000

    Include domain mapping

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,3 @@
-output "pool_name" {
-  description = "Workload Identity Pool name"
-  value       = module.github-workload-identity.pool_name
-}
-
-output "provider_name" {
-  description = "Workload Identity Provider name"
-  value       = module.github-workload-identity.provider_name
-}
-
 output "sa_email" {
   description = "Google Service Account email"
   value       = module.github-service-account.email
