c8ecf37e72ad8788ea263b41a3a3966bb5be5bcb
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2025-04-30 11:48:01 +0200

    Remove unused imports

--- map-routes/src/commonMain/kotlin/io/ashdavies/routes/CircuitConfig.kt
+++ map-routes/src/commonMain/kotlin/io/ashdavies/routes/CircuitConfig.kt
@@ -3,8 +3,6 @@ package io.ashdavies.routes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import com.slack.circuit.foundation.Circuit
-import io.ashdavies.content.PlatformContext
-import io.ktor.client.HttpClient
 
 @Composable
 internal fun rememberCircuit(graph: RoutesGraph): Circuit = remember(graph) {
