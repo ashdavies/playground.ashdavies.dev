baf56b7eff5bd70cb802233c6cfb25aede8f4ecd
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2025-05-07 00:26:55 +0200

    Include onepassword as terraform provider

---
+++ terraform/secrets.tf
@@ -0,0 +1,16 @@
+resource "onepassword_item" "Playground Secrets" {
+  vault    = var.op_vault
+
+  title    = "Playground Secrets"
+  category = "password"
+
+  section {
+    label = "Google"
+
+    field {
+      label = "Workload Identity"
+      type  = "CONCEALED"
+      value = module.github-workload-identity.provider_name
+    }
+  }
+}
