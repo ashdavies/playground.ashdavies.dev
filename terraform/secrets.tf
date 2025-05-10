9499fe774cea1dd86501ae19128d6f0ba75a1ad2
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2025-05-10 16:14:55 +0200

    Migrate secrets to 1password

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -5,7 +5,23 @@ resource "onepassword_item" "playground_secrets" {
   category = "secure_note"
 
   section {
+    label = "Firebase"
+
+    field {
+      label = "Android App ID"
+      type  = "CONCEALED"
+      value = google_firebase_android_app.release.app_id
+    }
+  }
+
+  section {
     label = "Google"
+
+    field {
+      label = "Service Account ID"
+      type  = "CONCEALED"
+      value = module.github-service-account.email
+    }
 
     field {
       label = "Workload Identity"
