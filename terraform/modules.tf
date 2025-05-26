7d16580a722267f8fb1d12e5fefb873fd76284a3
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2025-05-26 13:59:56 +0200

    Under no circumstances run terraform apply on PRs (#1677)
    * Under no circumstances run a Terraform plan on PRs
    
    * Bump terraform version
    
    * Remove non-existing labels
    
    * chore(deps): update terraform version to 1.12.1
    
    Updated the Terraform version from 1.12.0 to 1.12.1 in the GitHub Actions workflow.

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -55,23 +55,6 @@ module "github-repository" {
   source      = "./modules/github/repository"
   repository  = var.gh_repo_name
   description = "Playground"
-  labels      = [
-    {
-      description = "Indicates an unexpected problem or unintended behavior",
-      color       = "D73A4A"
-      name        = "Bug",
-    },
-    {
-      name        = "Enhancement"
-      description = "Indicates new feature requests"
-      color       = "c5def5"
-    },
-    {
-      name        = "Terraform"
-      description = "Execute terraform workflow"
-      color       = "e99695"
-    }
-  ]
   topics = [
     "compose-multiplatform",
     "kotlin-multiplatform",
