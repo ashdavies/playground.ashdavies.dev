8b295ad86525bd087a15fc90f38d70ad03526670
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2025-07-24 22:45:31 +0200

    Create workflow identity provider secret (#1828)
    * Create workflow identity provider secret
    
    * Store value as encrypted secret

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,3 +1,9 @@
+resource "github_actions_secret" "workload_identity_provider" {
+  repository      = var.gh_repo_name
+  secret_name     = "WORKLOAD_IDENTITY_PROVIDER"
+  encrypted_value = module.github-workload-identity.provider_name
+}
+
 resource "onepassword_item" "playground_secrets" {
   vault = var.op_vault_uuid
 
@@ -23,6 +29,7 @@ resource "onepassword_item" "playground_secrets" {
       value = module.github-service-account.email
     }
 
+    # Deprecated: This value is now stored as the WORKLOAD_IDENTITY_PROVIDER GitHub Actions secret.
     field {
       label = "Workload Identity"
       type  = "CONCEALED"
