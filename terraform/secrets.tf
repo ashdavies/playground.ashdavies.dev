f0eab702b2208b0392ec8db56a22b496bf3b2b0d
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2025-07-26 15:31:38 +0200

    Firebase android app id secret (#1834)
    * Firebase android app id secret
    
    * Move google services to terraform
    
    * Decode google services
    
    * Ignore unused resource

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,7 +1,13 @@
-resource "github_actions_secret" "workload_identity_provider" {
+resource "github_actions_secret" "firebase_android_app_id" {
   repository      = var.gh_repo_name
-  secret_name     = "WORKLOAD_IDENTITY_PROVIDER"
-  plaintext_value = module.github-workload-identity.provider_name
+  secret_name     = "FIREBASE_ANDROID_APP_ID"
+  plaintext_value = google_firebase_android_app.release.app_id
+}
+
+resource "github_actions_secret" "firebase_google_services" {
+  repository      = var.gh_repo_name
+  secret_name     = "FIREBASE_GOOGLE_SERVICES"
+  plaintext_value = data.google_firebase_android_app_config.release.config_file_contents
 }
 
 resource "github_actions_secret" "google_service_account_id" {
@@ -10,19 +16,8 @@ resource "github_actions_secret" "google_service_account_id" {
   plaintext_value = module.github-service-account.email
 }
 
-resource "onepassword_item" "playground_secrets" {
-  vault = var.op_vault_uuid
-
-  title    = "Playground Secrets"
-  category = "secure_note"
-
-  section {
-    label = "Firebase"
-
-    field {
-      label = "Android App ID"
-      type  = "CONCEALED"
-      value = google_firebase_android_app.release.app_id
-    }
-  }
+resource "github_actions_secret" "workload_identity_provider" {
+  repository      = var.gh_repo_name
+  secret_name     = "WORKLOAD_IDENTITY_PROVIDER"
+  plaintext_value = module.github-workload-identity.provider_name
 }
