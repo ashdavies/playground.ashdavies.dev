ce28f8c780aeb7f47ea3443d6911a52676d095d8
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2025-08-04 11:16:26 +0200

    Move scaffold to dedicated presenter (#1864)
    * Move scaffold to dedicated presenter
    
    * Resolve index failure

--- tally-app/src/commonMain/kotlin/io/ashdavies/tally/upcoming/UpcomingScreen.kt
+++ tally-app/src/commonMain/kotlin/io/ashdavies/tally/upcoming/UpcomingScreen.kt
@@ -1,35 +1,10 @@
 package io.ashdavies.tally.upcoming
 
-import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
-import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
-import androidx.compose.material3.adaptive.layout.AnimatedPane
-import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
-import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
-import androidx.compose.material3.adaptive.layout.calculatePaneScaffoldDirective
-import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
-import androidx.compose.material3.windowsizeclass.WindowSizeClass
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.backhandler.BackHandler
-import androidx.compose.ui.unit.dp
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.screen.Screen
-import com.slack.circuit.runtime.ui.Ui
-import dev.zacsweers.metro.AppScope
-import dev.zacsweers.metro.ContributesIntoMap
-import dev.zacsweers.metro.Inject
-import dev.zacsweers.metro.binding
 import io.ashdavies.parcelable.Parcelable
 import io.ashdavies.parcelable.Parcelize
-import io.ashdavies.tally.circuit.CircuitScreenKey
-import io.ashdavies.tally.events.EventsDetailScreen
-import io.ashdavies.tally.events.EventsDetailUi
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.coroutines.launch
 import io.ashdavies.tally.events.Event as DbConference
 
 @Parcelize
@@ -45,64 +20,4 @@ internal object UpcomingScreen : Parcelable, Screen {
         val errorMessage: String?,
         val eventSink: (Event) -> Unit,
     ) : CircuitUiState
-}
-
-@CircuitScreenKey(UpcomingScreen::class)
-@ContributesIntoMap(AppScope::class, binding<Ui<*>>())
-internal class UpcomingUi @Inject constructor(
-    private val eventsDetailUi: EventsDetailUi,
-) : Ui<UpcomingScreen.State> {
-
-    @Composable
-    @OptIn(ExperimentalMaterial3AdaptiveApi::class)
-    override fun Content(state: UpcomingScreen.State, modifier: Modifier) {
-        val scaffoldDirective = calculatePaneScaffoldDirective(currentWindowAdaptiveInfo())
-        val scaffoldNavigator = rememberListDetailPaneScaffoldNavigator<Int>(
-            scaffoldDirective = scaffoldDirective.copy(
-                horizontalPartitionSpacerSize = 0.dp,
-            ),
-        )
-
-        val coroutineScope = rememberCoroutineScope()
-        val onBack by rememberUpdatedState<() -> Unit> {
-            coroutineScope.launch { scaffoldNavigator.navigateBack() }
-        }
-
-        @OptIn(ExperimentalComposeUiApi::class)
-        BackHandler(scaffoldNavigator.canNavigateBack(), onBack)
-
-        ListDetailPaneScaffold(
-            directive = scaffoldNavigator.scaffoldDirective,
-            value = scaffoldNavigator.scaffoldValue,
-            listPane = {
-                AnimatedPane {
-                    UpcomingPane(
-                        state = state,
-                        onClick = {
-                            coroutineScope.launch {
-                                scaffoldNavigator.navigateTo(
-                                    pane = ListDetailPaneScaffoldRole.Detail,
-                                    contentKey = it,
-                                )
-                            }
-                        },
-                    )
-                }
-            },
-            detailPane = {
-                AnimatedPane {
-                    scaffoldNavigator.currentDestination?.contentKey?.let {
-                        eventsDetailUi.Content(
-                            EventsDetailScreen.State(
-                                itemState = EventsDetailScreen.State.ItemState.Done(requireNotNull(state.itemList[it])),
-                                onBackPressed = onBack,
-                            ),
-                            modifier = Modifier,
-                        )
-                    }
-                }
-            },
-            modifier = modifier,
-        )
-    }
 }
