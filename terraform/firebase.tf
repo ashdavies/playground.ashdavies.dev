b223da2233d30e4d8e986ea8e3e91be079a2e33a
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2025-08-12 20:59:36 +0200

    Fix Terraform Firebase app (#1886)
    * Fix Terraform firebase app
    
    * Store google json for debugging
    
    * Update pull-request.yml

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -1,8 +1,8 @@
 resource "google_firebase_android_app" "release" {
   provider      = google-beta
   project       = var.project_id
-  display_name  = "Playground Debug"
-  package_name  = "dev.ashdavies.playground.debug"
+  display_name  = "Playground Release"
+  package_name  = "dev.ashdavies.playground"
   sha1_hashes   = ["a75f572e40a926bb1708178c134a1e86faadaa09"]
   sha256_hashes = ["0442b951b2d82cb339a7e4ef9469e71e89b182c7be5efb119f587cc2b19995bc"]
 }
