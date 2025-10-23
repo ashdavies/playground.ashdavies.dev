313956bc9d5103e2ba7e840938a898157661796f
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2025-10-23 12:22:27 +0200

    Fix remote variable reference in base branch determination (#2060)

--- scripts/github_pull_request.sh
+++ scripts/github_pull_request.sh
@@ -76,10 +76,9 @@ fi
 
 GIT_REPO="$(gh repo view --json nameWithOwner -q .nameWithOwner)"
 BRANCH_NAME="auto/$(uuidgen)"
-REMOTE="${REMOTE:-origin}"
 
 # Determine base branch (e.g. main/master)
-BASE_BRANCH="$(git symbolic-ref "refs/remotes/${REMOTE}/HEAD" | awk -F/ '{print $NF}')"
+BASE_BRANCH="$(git symbolic-ref "refs/remotes/${remote:-origin}/HEAD" | awk -F/ '{print $NF}')"
 BASE_SHA="$(gh api "repos/$GIT_REPO/git/ref/heads/$BASE_BRANCH" --jq .object.sha)"
 
 log "Repository: $GIT_REPO"
