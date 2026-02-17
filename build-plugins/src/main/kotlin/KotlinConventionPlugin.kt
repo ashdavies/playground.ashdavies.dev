def9302b906cadb015051f2d421725ece1b882cc
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2026-02-17 23:38:05 +0100

    Convention plugin cleanup

--- build-plugins/src/main/kotlin/KotlinConventionPlugin.kt
+++ build-plugins/src/main/kotlin/KotlinConventionPlugin.kt
@@ -1,26 +1,20 @@
-import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
-import org.jlleitschuh.gradle.ktlint.KtlintPlugin
 
 public class KotlinConventionPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         plugins.apply(libs.plugins.kotlin.serialization)
         plugins.apply(libs.plugins.kotlin.multiplatform)
 
-        apply<DetektPlugin>()
-        apply<KtlintPlugin>()
+        plugins.apply(libs.plugins.detekt)
+        plugins.apply(libs.plugins.ktlint)
 
         extensions.configure<KotlinMultiplatformExtension> {
             explicitApi()
-            jvm()
         }
 
         dependencies.add("detektPlugins", libs.detekt.compose)
@@ -32,13 +26,6 @@ public class KotlinConventionPlugin : Plugin<Project> {
 
             val detektPlugin = libs.plugins.detekt.get()
             toolVersion = "${detektPlugin.version}"
-        }
-
-        tasks.withType(KotlinCompile::class.java).configureEach {
-            compilerOptions {
-                val jvmTargetVersion = libs.versions.kotlin.jvmTarget.get()
-                jvmTarget.set(JvmTarget.fromTarget(jvmTargetVersion))
-            }
         }
 
         extensions.configure<KtlintExtension> {
