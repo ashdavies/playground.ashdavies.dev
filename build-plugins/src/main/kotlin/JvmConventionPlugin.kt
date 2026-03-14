64d608a6f176332a530842d1bfd77dcfa2a2d814
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2026-03-14 23:58:42 +0100

    Clean up workflows and enhance JVM convention plugin
    Remove unused setup-java action from the publish-release workflow and enable Kotlin Multiplatform plugin in the JvmConventionPlugin.

--- build-plugins/src/main/kotlin/JvmConventionPlugin.kt
+++ build-plugins/src/main/kotlin/JvmConventionPlugin.kt
@@ -4,9 +4,12 @@ import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
+@Suppress("unused")
 @OptIn(ExperimentalKotlinGradlePluginApi::class)
 internal class JvmConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
+        plugins.apply(libs.plugins.kotlin.multiplatform)
+
         extensions.configure<KotlinMultiplatformExtension> {
             jvm()
         }
