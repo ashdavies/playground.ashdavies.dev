18cd37b22f2472bbaefad3cab40984f0759c5d2a
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2026-06-20 17:24:21 +0200

    Rename app nickname (#2502)

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -1,7 +1,7 @@
 resource "google_firebase_android_app" "release" {
   provider      = google-beta
   project       = var.project_id
-  display_name  = "Playground Release"
+  display_name  = "Android Release"
   package_name  = "dev.ashdavies.playground"
   sha1_hashes   = ["a75f572e40a926bb1708178c134a1e86faadaa09"]
   sha256_hashes = ["0442b951b2d82cb339a7e4ef9469e71e89b182c7be5efb119f587cc2b19995bc"]
