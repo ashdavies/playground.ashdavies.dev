7fb01cde247a48e59c892d2bdae4fca7253d00fd
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2026-07-26 14:59:58 +0200

    Use terraform field_map property

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,19 +1,14 @@
-locals {
-  github_app_id = { for it in data.onepassword_item.github_developer_application.section[0].field : it.label => sensitive(it.value) }["app id"]
-  keystore_password = {for it in data.onepassword_item.android_keystore.section[0].field :it.label => sensitive(it.value)}["password"]
-}
-
 resource "github_actions_secret" "main" {
   for_each = {
     BROWSER_API_KEY              = module.browser_api_key.key_string
     BROWSER_APP_ID               = google_firebase_web_app.browser.app_id
     FASTLANE_SERVICE_ACCOUNT_KEY = google_service_account_key.fastlane_supply_key.private_key
     FIREBASE_GOOGLE_SERVICES     = data.google_firebase_android_app_config.android_release.config_file_contents
-    GH_APP_ID                    = local.github_app_id
+    GH_APP_ID                    = data.onepassword_item.github_developer_application.section_map[""].field_map["app id"].value
     GH_PRIVATE_KEY               = base64encode(data.onepassword_item.github_developer_application.private_key)
     GOOGLE_SERVICE_ACCOUNT_ID    = module.github_service_account.email
     KEYSTORE_FILE                = data.onepassword_item.android_keystore.file[0].content_base64
-    KEYSTORE_PASSWORD            = local.keystore_password
+    KEYSTORE_PASSWORD            = data.onepassword_item.android_keystore.section_map[""].field_map["password"].value
     OP_SERVICE_ACCOUNT_TOKEN     = var.op_service_account_token
     WORKLOAD_IDENTITY_PROVIDER   = module.github_workload_identity.provider_name
   }
