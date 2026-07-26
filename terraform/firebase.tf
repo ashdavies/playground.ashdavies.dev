7f69a5ab72723900a7dcf134b62e12e207306033
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2026-07-26 18:55:53 +0200

    Update sha256_hashes for Firebase configuration

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -4,7 +4,7 @@ resource "google_firebase_android_app" "android_release" {
   display_name  = "Android Release"
   package_name  = "dev.ashdavies.playground"
   sha1_hashes   = ["a75f572e40a926bb1708178c134a1e86faadaa09"]
-  sha256_hashes = ["0442b951b2d82cb339a7e4ef9469e71e89b182c7be5efb119f587cc2b19995bc"]
+  sha256_hashes = ["c5ecb9fa2a121660be5d8d7953c81338ef8db15b3616f241f5c6fa005da0387a"]
 }
 
 resource "google_firebase_web_app" "browser" {
