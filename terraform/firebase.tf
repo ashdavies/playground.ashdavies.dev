be26c7b8a8fb409cdac3d9c09e4febb2d4241c4c
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2026-07-26 19:49:12 +0200

    Include release key signature (#2578)

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -3,8 +3,10 @@ resource "google_firebase_android_app" "android_release" {
   project       = var.project_id
   display_name  = "Android Release"
   package_name  = "dev.ashdavies.playground"
-  sha1_hashes   = ["a75f572e40a926bb1708178c134a1e86faadaa09"]
-  sha256_hashes = ["c5ecb9fa2a121660be5d8d7953c81338ef8db15b3616f241f5c6fa005da0387a"]
+  sha256_hashes = [
+    "9e271f4a830ca768f32575191aee616c25e6c5724485cb38e9908212e7915ef9",
+    "c5ecb9fa2a121660be5d8d7953c81338ef8db15b3616f241f5c6fa005da0387a"
+  ]
 }
 
 resource "google_firebase_web_app" "browser" {
