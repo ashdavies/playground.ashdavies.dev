2859216638eb58f8c71dfa2b5e3df20151ca45e0
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2026-08-14 21:03:03 +0200

    rolling back expressive as android artifact is unreliable

--- android-app/src/test/kotlin/dev/ashdavies/playground/tooling/MaterialPreviewTheme.kt
+++ android-app/src/test/kotlin/dev/ashdavies/playground/tooling/MaterialPreviewTheme.kt
@@ -1,7 +1,6 @@
 package dev.ashdavies.playground.tooling
 
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -10,12 +9,11 @@ import dev.ashdavies.playground.material.dynamicColorScheme
 import org.jetbrains.compose.resources.PreviewContextConfigurationEffect
 
 @Composable
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 internal fun MaterialPreviewTheme(content: @Composable () -> Unit) {
     CompositionLocalProvider(LocalInspectionMode provides true) {
         PreviewContextConfigurationEffect()
 
-        MaterialExpressiveTheme(dynamicColorScheme()) {
+        MaterialTheme(dynamicColorScheme()) {
             Surface(content = content)
         }
     }
